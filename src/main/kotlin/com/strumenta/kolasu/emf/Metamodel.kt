51705ec978b74331379ee7fcf98ff1719cb30265
Federico Tomassetti <federico@tomassetti.me>
2021-03-05 18:01:09 +0100

    generation of ecore from Kolasu metamodel

--- src/main/kotlin/com/strumenta/kolasu/emf/Metamodel.kt
+++ src/main/kotlin/com/strumenta/kolasu/emf/Metamodel.kt
@@ -1,48 +1,111 @@
 package com.strumenta.kolasu.emf
 
+import com.strumenta.kolasu.model.Node
 import com.strumenta.kolasu.model.processProperties
+import org.eclipse.emf.common.util.URI
 import org.eclipse.emf.ecore.EClass
+import org.eclipse.emf.ecore.EDataType
 import org.eclipse.emf.ecore.EPackage
 import org.eclipse.emf.ecore.EcoreFactory
+import org.eclipse.emf.ecore.resource.Resource
+import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl
+import org.eclipse.emf.ecore.xmi.impl.EcoreResourceFactoryImpl
+import java.io.File
 import java.util.*
+import kotlin.collections.HashMap
 import kotlin.reflect.KClass
+import kotlin.reflect.KType
+import kotlin.reflect.full.superclasses
 import kotlin.reflect.jvm.jvmErasure
 
 class MetamodelBuilder(packageName: String) {
 
     private val ePackage : EPackage
-    private val considered = LinkedList<KClass<*>>()
+    private val eClasses = HashMap<KClass<*>, EClass>()
+    private val dataTypes = HashMap<KType, EDataType>()
 
     init {
         ePackage = EcoreFactory.eINSTANCE.createEPackage()
         ePackage.name = packageName
     }
 
+    private fun toEDataType(ktype: KType) : EDataType {
+        if (!dataTypes.containsKey(ktype)) {
+            val eDataType = EcoreFactory.eINSTANCE.createEDataType()
+            if (ktype.classifier == String::class) {
+                eDataType.name = "String"
+                eDataType.instanceClass = String::class.java
+            } else {
+                TODO(ktype.toString())
+            }
+            ePackage.eClassifiers.add(eDataType)
+            dataTypes[ktype] = eDataType
+        }
+        return dataTypes[ktype]!!
+    }
+
     private fun toEClass(kClass: KClass<*>) : EClass {
         val eClass = EcoreFactory.eINSTANCE.createEClass()
+        kClass.superclasses.forEach {
+            if (it != Any::class && it != Node::class) {
+                eClass.eSuperTypes.add(addClass(it))
+            }
+        }
         eClass.name = kClass.simpleName
-        eClass.isAbstract = kClass.isAbstract
+        eClass.isAbstract = kClass.isAbstract || kClass.isSealed
+        kClass.java.processProperties {
+            if (it.provideNodes) {
+                val ec = EcoreFactory.eINSTANCE.createEReference()
+                ec.name = it.name
+                if (it.multiple) {
+                    ec.lowerBound = 0
+                    ec.upperBound = -1
+                } else {
+                    ec.lowerBound = 0
+                    ec.upperBound = 1
+                }
+                ec.isContainment = true
+                ec.eType = addClass(it.valueType.classifier as KClass<*>)
+                eClass.eReferences.add(ec)
+            } else {
+                val ea = EcoreFactory.eINSTANCE.createEAttribute()
+                ea.name = it.name
+                if (it.multiple) {
+                    ea.lowerBound = 0
+                    ea.upperBound = -1
+                } else {
+                    ea.lowerBound = 0
+                    ea.upperBound = 1
+                }
+                ea.eType = toEDataType(it.valueType)
+                eClass.eAttributes.add(ea)
+            }
+        }
         return eClass
     }
 
-    fun addClass(kClass: KClass<*>) {
-        if (considered.contains(kClass)) {
-            return
-        }
-        kClass.java.processProperties {
-            if (it.provideNodes) {
-                println(it.valueType.jvmErasure)
-                addClass(it.valueType.jvmErasure)
+    fun addClass(kClass: KClass<*>) : EClass {
+        if (!eClasses.containsKey(kClass)) {
+            val eClass = toEClass(kClass)
+            ePackage.eClassifiers.add(eClass)
+            eClasses[kClass] = eClass
+            if (kClass.isSealed) {
+                kClass.sealedSubclasses.forEach { addClass(it) }
             }
         }
-        if (kClass.isSealed) {
-            kClass.sealedSubclasses.forEach { addClass(it) }
-        }
-        ePackage.eClassifiers.add(toEClass(kClass))
-        considered.add(kClass)
+        return eClasses[kClass]!!
     }
 
     fun generate() : EPackage {
         return ePackage
     }
+}
+
+fun EPackage.saveEcore(ecoreFile: File) {
+    val resourceSet = ResourceSetImpl()
+    resourceSet.resourceFactoryRegistry.extensionToFactoryMap["ecore"] = EcoreResourceFactoryImpl()
+    val uri: URI = URI.createFileURI(ecoreFile.absolutePath)
+    val resource: Resource = resourceSet.createResource(uri)
+    resource.contents.add(this)
+    resource.save(null)
 }
--- src/test/kotlin/com/strumenta/kolasu/emf/MetamodelTest.kt
+++ src/test/kotlin/com/strumenta/kolasu/emf/MetamodelTest.kt
@@ -3,6 +3,7 @@ package com.strumenta.kolasu.emf
 import com.strumenta.kolasu.model.Node
 import org.eclipse.emf.ecore.EClass
 import org.junit.Test
+import java.io.File
 import kotlin.test.assertEquals
 
 sealed class Statement : Node()
@@ -18,14 +19,26 @@ class MetamodelTest {
         val metamodelBuilder = MetamodelBuilder("SimpleMM")
         metamodelBuilder.addClass(CompilationUnit::class)
         var ePackage = metamodelBuilder.generate()
+        ePackage.saveEcore(File("simplemm.ecore"))
         assertEquals("SimpleMM", ePackage.name)
-        assertEquals(5, ePackage.eClassifiers.size)
+        assertEquals(6, ePackage.eClassifiers.size)
 
         val cu : EClass = ePackage.eClassifiers.find { it.name == "CompilationUnit" } as EClass
+        assertEquals(0, cu.eAllSuperTypes.size)
         assertEquals(0, cu.eAllAttributes.size)
+        assertEquals(1, cu.eAllContainments.size)
+        assertEquals(1, cu.eAllReferences.size)
+        assertEquals(1, cu.eAllStructuralFeatures.size)
+
+        val e : EClass = ePackage.eClassifiers.find { it.name == "Expression" } as EClass
+        assertEquals(true, e.isAbstract)
 
         val sl : EClass = ePackage.eClassifiers.find { it.name == "StringLiteral" } as EClass
+        assertEquals(1, sl.eAllSuperTypes.size)
         assertEquals(1, sl.eAllAttributes.size)
+        assertEquals(0, sl.eAllContainments.size)
+        assertEquals(0, sl.eAllReferences.size)
+        assertEquals(1, sl.eAllStructuralFeatures.size)
 
         val vd : EClass = ePackage.eClassifiers.find { it.name == "VarDeclaration" } as EClass
         assertEquals(1, vd.eAllAttributes.size)
