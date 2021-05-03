54b9c8a2e61f0857d726536b90df57f8acd1b83e
Federico Tomassetti <federico@tomassetti.me>
2021-05-03 17:59:26 +0200

    revising ParserBenchExampleGenerator

--- src/main/kotlin/com/strumenta/kolasu/parserbench/ParserBenchExampleGenerator.kt
+++ src/main/kotlin/com/strumenta/kolasu/parserbench/ParserBenchExampleGenerator.kt
@@ -2,14 +2,29 @@ package com.strumenta.kolasu.parserbench
 
 import com.google.gson.JsonObject
 import com.google.gson.JsonPrimitive
+import com.strumenta.kolasu.emf.saveAsJson
 import com.strumenta.kolasu.emf.saveAsJsonObject
 import com.strumenta.kolasu.emf.toEObject
 import com.strumenta.kolasu.parsing.Parser
 import org.eclipse.emf.ecore.EPackage
+import java.io.File
+import java.io.FileWriter
 
-class ParserBenchExampleGenerator(val parser : Parser<*>, val ePackage: EPackage) {
+class ParserBenchExampleGenerator(val parser : Parser<*>, val ePackage: EPackage, val directory: File) {
 
-    fun generate(name: String, code: String) {
+    fun generateMetamodel() {
+        ePackage.saveAsJson(File(directory, "metamodel.json"))
+    }
+
+    fun generateExample(file: File) {
+        generateExample(file.nameWithoutExtension, file.readText())
+    }
+
+    fun generateExample(name: String, file: File) {
+        generateExample(name, file.readText())
+    }
+
+    fun generateExample(name: String, code: String) {
         val jo = JsonObject()
         jo.add("name", JsonPrimitive(name))
         jo.add("code", JsonPrimitive(code))
@@ -20,5 +35,10 @@ class ParserBenchExampleGenerator(val parser : Parser<*>, val ePackage: EPackage
         }
         val astEMF = parsingResult.root!!.toEObject(ePackage)
         jo.add("ast", astEMF.saveAsJsonObject())
+        jo.toString()
+        val file = File(directory, "$name.json")
+        val fw = FileWriter(file)
+        fw.write(jo.toString())
+        fw.close()
     }
 }
