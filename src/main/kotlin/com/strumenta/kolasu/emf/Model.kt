2d619a103cac4298ba677ff4645ef7f828d0b5c0
Alessio Stalla <alessiostalla@gmail.com>
2021-05-24 17:32:42 +0200

    Remove print statement

--- src/main/kotlin/com/strumenta/kolasu/emf/Model.kt
+++ src/main/kotlin/com/strumenta/kolasu/emf/Model.kt
@@ -180,8 +180,6 @@ fun EObject.saveAsJson(): String {
     val uri: URI = URI.createURI("dummy-URI")
     val resource: Resource = JsonResourceFactory().createResource(uri)
     resource.contents.add(this)
-    val uf = resource.getURIFragment(this)
-    println(uf)
     val output = ByteArrayOutputStream()
     resource.save(output, null)
     return output.toString(Charsets.UTF_8.name())
