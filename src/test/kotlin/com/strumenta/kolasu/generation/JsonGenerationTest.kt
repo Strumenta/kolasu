9b79500a43515feafd42e9d93a27d3400eb465e0
Federico Tomassetti <federico@tomassetti.me>
2021-06-25 15:08:48 +0200

    fix tests

--- src/test/kotlin/com/strumenta/kolasu/generation/JsonGenerationTest.kt
+++ src/test/kotlin/com/strumenta/kolasu/generation/JsonGenerationTest.kt
@@ -53,20 +53,20 @@ class JsonGenerationTest {
         val json = JsonGenerator().generateString(myRoot)
         assertEquals(
             """{
-  "type": "com.strumenta.kolasu.generation.MyRoot",
+  "#type": "com.strumenta.kolasu.generation.MyRoot",
   "mainSection": {
-    "type": "com.strumenta.kolasu.generation.Section",
+    "#type": "com.strumenta.kolasu.generation.Section",
     "contents": [
       {
-        "type": "com.strumenta.kolasu.generation.Content",
+        "#type": "com.strumenta.kolasu.generation.Content",
         "id": 1
       },
       {
-        "type": "com.strumenta.kolasu.generation.Content",
+        "#type": "com.strumenta.kolasu.generation.Content",
         "annidatedContent": {
-          "type": "com.strumenta.kolasu.generation.Content",
+          "#type": "com.strumenta.kolasu.generation.Content",
           "annidatedContent": {
-            "type": "com.strumenta.kolasu.generation.Content",
+            "#type": "com.strumenta.kolasu.generation.Content",
             "id": 4
           },
           "id": 3
@@ -98,7 +98,7 @@ class JsonGenerationTest {
         JsonGenerator().generateJSONWithStreaming(myRoot, JsonWriter(writer))
         val json = writer.toString()
         assertEquals(
-            """{"type":"MyRoot","mainSection":{"type":"Section","contents":[{"type":"Content","annidatedContent":null,"id":1},{"type":"Content","annidatedContent":{"type":"Content","annidatedContent":{"type":"Content","annidatedContent":null,"id":4},"id":3},"id":2}],"name":"Section1"},"otherSections":[]}""",
+            """{"#type":"com.strumenta.kolasu.generation.MyRoot","mainSection":{"#type":"com.strumenta.kolasu.generation.Section","contents":[{"#type":"com.strumenta.kolasu.generation.Content","annidatedContent":null,"id":1},{"#type":"com.strumenta.kolasu.generation.Content","annidatedContent":{"#type":"com.strumenta.kolasu.generation.Content","annidatedContent":{"#type":"com.strumenta.kolasu.generation.Content","annidatedContent":null,"id":4},"id":3},"id":2}],"name":"Section1"},"otherSections":[]}""",
             json
         )
     }
