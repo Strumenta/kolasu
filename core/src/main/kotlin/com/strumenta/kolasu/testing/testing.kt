729b20737f65c288aba82e3685816af5190bb9cc
Federico Tomassetti <federico@tomassetti.me>
2021-07-16 16:30:23 +0200

    formatting

--- core/src/main/kotlin/com/strumenta/kolasu/testing/testing.kt
+++ core/src/main/kotlin/com/strumenta/kolasu/testing/testing.kt
@@ -5,8 +5,12 @@ import org.antlr.v4.runtime.ParserRuleContext
 import org.antlr.v4.runtime.Vocabulary
 import org.junit.jupiter.api.Assertions.assertEquals
 
-fun assertParseTreeStr(expectedMultiLineStr: String, root: ParserRuleContext, vocabulary: Vocabulary,
-                       printParseTree: Boolean = true) {
+fun assertParseTreeStr(
+    expectedMultiLineStr: String,
+    root: ParserRuleContext,
+    vocabulary: Vocabulary,
+    printParseTree: Boolean = true
+) {
     val actualParseTree = toParseTree(root, vocabulary).multiLineString()
     if (printParseTree) {
         println("parse tree:\n\n${actualParseTree}\n")
