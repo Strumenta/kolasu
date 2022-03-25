33551d363a9640f093590740aa1f9e5dcbf1a868
Lorenzo Addazi <lorenzo.addazi@strumenta.com>
2022-03-25 15:29:51 +0100

    Strumenta#66 - Printing AST - Adds configuration for excluding/including properties based on their visibility

--- core/src/main/kotlin/com/strumenta/kolasu/model/Printing.kt
+++ core/src/main/kotlin/com/strumenta/kolasu/model/Printing.kt
@@ -1,8 +1,13 @@
 package com.strumenta.kolasu.model
 
 import java.lang.reflect.ParameterizedType
+import kotlin.reflect.KProperty1
+import kotlin.reflect.KVisibility.INTERNAL
+import kotlin.reflect.KVisibility.PRIVATE
+import kotlin.reflect.KVisibility.PROTECTED
 import kotlin.reflect.KVisibility.PUBLIC
 import kotlin.reflect.full.memberProperties
+import kotlin.reflect.jvm.isAccessible
 import kotlin.reflect.jvm.javaType
 
 private const val indentBlock = "  "
@@ -23,8 +28,21 @@ data class DebugPrintConfiguration(
     val skipEmptyCollections: Boolean = false,
     val skipNull: Boolean = false,
     val forceShowPosition: Boolean = false,
-    val hide: List<String> = emptyList()
+    val hide: List<String> = emptyList(),
+    val skipPrivateProperties: Boolean = true,
+    val skipProtectedProperties: Boolean = true,
+    val skipInternalProperties: Boolean = true,
+    val skipPublicProperties: Boolean = false
 )
+
+private fun KProperty1<Node, *>.hasRelevantVisibility(configuration: DebugPrintConfiguration): Boolean {
+    return when (requireNotNull(this.visibility)) {
+        PRIVATE -> !configuration.skipPrivateProperties
+        PROTECTED -> !configuration.skipProtectedProperties
+        INTERNAL -> !configuration.skipInternalProperties
+        PUBLIC -> !configuration.skipPublicProperties
+    }
+}
 
 private fun Node.showSingleAttribute(indent: String, sb: StringBuilder, propertyName: String, value: Any?) {
     sb.append("$indent$indentBlock$propertyName = ${value}\n")
@@ -50,8 +68,9 @@ fun Node.debugPrint(indent: String = "", configuration: DebugPrintConfiguration 
             } else {
                 val mt = property.returnType.javaType
                 if (mt is ParameterizedType && mt.rawType == List::class.java) {
-                    if (property.visibility == PUBLIC) {
-                        if (property.get(this) == null) {
+                    if (property.hasRelevantVisibility(configuration)) {
+                        property.isAccessible = true
+                        if (property.get(this) == null && !configuration.skipNull) {
                             sb.append("$indent$indentBlock${property.name} = null")
                         } else {
                             val value = property.get(this) as List<*>
@@ -84,7 +103,8 @@ fun Node.debugPrint(indent: String = "", configuration: DebugPrintConfiguration 
                         }
                     }
                 } else {
-                    if (property.visibility == PUBLIC) {
+                    if (property.hasRelevantVisibility(configuration)) {
+                        property.isAccessible = true
                         val value = property.get(this)
                         if (value == null && configuration.skipNull) {
                             // nothing to do
