67e17858ab8d45e72cef57a78ee7e9b1c6e39ba5
Alessio Stalla <alessiostalla@gmail.com>
2022-06-01 15:35:12 +0200

    Linting

--- core/src/main/kotlin/com/strumenta/kolasu/mapping/ParseTreeToASTTransformer.kt
+++ core/src/main/kotlin/com/strumenta/kolasu/mapping/ParseTreeToASTTransformer.kt
@@ -2,7 +2,6 @@ package com.strumenta.kolasu.mapping
 
 import com.strumenta.kolasu.model.Node
 import com.strumenta.kolasu.model.Origin
-import com.strumenta.kolasu.model.Position
 import com.strumenta.kolasu.parsing.ParseTreeOrigin
 import com.strumenta.kolasu.parsing.withParseTreeNode
 import com.strumenta.kolasu.transfomation.ASTTransformer
@@ -10,12 +9,11 @@ import com.strumenta.kolasu.validation.Issue
 import org.antlr.v4.runtime.ParserRuleContext
 import org.antlr.v4.runtime.tree.ParseTree
 
-
 /**
  * Implements a transformation from an ANTLR parse tree (the output of the parser) to an AST (a higher-level
  * representation of the source code).
  */
-open class ParseTreeToASTTransformer(issues: MutableList<Issue> = mutableListOf()): ASTTransformer(issues) {
+open class ParseTreeToASTTransformer(issues: MutableList<Issue> = mutableListOf()) : ASTTransformer(issues) {
     /**
      * Performs the transformation of a node and, recursively, its descendants. In addition to the overridden method,
      * it also assigns the parseTreeNode to the AST node so that it can keep track of its position.
@@ -23,7 +21,7 @@ open class ParseTreeToASTTransformer(issues: MutableList<Issue> = mutableListOf(
      */
     override fun transform(source: Any?, parent: Node?): Node? {
         val node = super.transform(source, parent)
-        if(node != null && node.origin == null && source is ParserRuleContext) {
+        if (node != null && node.origin == null && source is ParserRuleContext) {
             node.withParseTreeNode(source)
         }
         return node
