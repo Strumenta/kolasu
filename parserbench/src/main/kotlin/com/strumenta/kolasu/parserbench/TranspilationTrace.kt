e5afe329fccabbddd728a9e7c6244855901ceaaa
Federico Tomassetti <federico@tomassetti.me>
2022-06-03 16:08:32 +0200

    add serialization of TranspilationTrace

--- parserbench/src/main/kotlin/com/strumenta/kolasu/parserbench/TranspilationTrace.kt
+++ parserbench/src/main/kotlin/com/strumenta/kolasu/parserbench/TranspilationTrace.kt
@@ -1,6 +1,10 @@
 package com.strumenta.kolasu.parserbench
 
+import com.google.gson.GsonBuilder
+import com.google.gson.JsonElement
+import com.google.gson.JsonObject
 import com.strumenta.kolasu.model.Node
+import com.strumenta.kolasu.serialization.JsonGenerator
 
 /**
  * A transpilation trace can be visualized to demonstrate how the transpiler work.
@@ -9,3 +13,17 @@ class TranspilationTrace<S: Node, T: Node>(val originalCode: String,
                                            val sourceAST: S,
                                            val targetAST: T,
                                            val generatedCode: String)
+
+fun <S: Node, T: Node>TranspilationTrace<S, T>.toJson(): JsonElement {
+    val jo = JsonObject()
+    jo.addProperty("originalCode", originalCode)
+    jo.add("sourceAST", JsonGenerator().generateJSON(sourceAST))
+    jo.add("targetAST", JsonGenerator().generateJSON(targetAST))
+    jo.addProperty("generatedCode", generatedCode)
+    return jo
+}
+
+fun <S: Node, T: Node>TranspilationTrace<S, T>.toJsonString(): String {
+    val gson = GsonBuilder().setPrettyPrinting().create()
+    return gson.toJson(toJson())
+}
