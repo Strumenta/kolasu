353ada2921b17b25c056c46e48d513845b2ec136
Federico Tomassetti <federico@tomassetti.me>
2022-06-04 09:51:05 +0200

    update versions

--- parserbench/src/main/kotlin/com/strumenta/kolasu/parserbench/TranspilationTrace.kt
+++ parserbench/src/main/kotlin/com/strumenta/kolasu/parserbench/TranspilationTrace.kt
@@ -5,7 +5,9 @@ import com.strumenta.kolasu.model.Node
 /**
  * A transpilation trace can be visualized to demonstrate how the transpiler work.
  */
-class TranspilationTrace<S: Node, T: Node>(val originalCode: String,
-                                           val sourceAST: S,
-                                           val targetAST: T,
-                                           val generatedCode: String)
+class TranspilationTrace<S : Node, T : Node>(
+    val originalCode: String,
+    val sourceAST: S,
+    val targetAST: T,
+    val generatedCode: String
+)
