72fbe71d940aefdb69d509aae4c08509ca6738b1
Federico Tomassetti <federico@tomassetti.me>
2022-06-23 11:47:36 +0200

    linting

--- javalib/src/test/kotlin/Library.kt
+++ javalib/src/test/kotlin/Library.kt
@@ -2,5 +2,5 @@ package com.strumenta.kolasu.javalib
 
 import com.strumenta.kolasu.model.Node
 
-data class Library(val books : List<Book>) : Node()
+data class Library(val books: List<Book>) : Node()
 data class Book(val title: String, val numberOfPages: Int) : Node()
