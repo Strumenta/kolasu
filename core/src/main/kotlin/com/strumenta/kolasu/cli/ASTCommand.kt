1f9f5d40b0147e971b923b39416108ea8815952d
Federico Tomassetti <federico@tomassetti.me>
2022-07-08 09:28:37 +0200

    creating ASTCommand

---
+++ core/src/main/kotlin/com/strumenta/kolasu/cli/ASTCommand.kt
@@ -0,0 +1,13 @@
+package com.strumenta.kolasu.cli
+
+import com.github.ajalt.clikt.core.CliktCommand
+
+/**
+ * This command prints the AST on the console or on file.
+ * The formats are the debugging format, JSON, XML, or EMF-JSON.
+ */
+open class ASTCommand : CliktCommand() {
+    override fun run() {
+        TODO("Not yet implemented")
+    }
+}
