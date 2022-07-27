951977b4b84cec2aaadd5fbc700b6fcf95e66f7f
Federico Tomassetti <federico@tomassetti.me>
2022-07-27 16:46:08 +0200

    adding examples to verify how duplicate properties are handled

--- core/src/test/kotlin/com/strumenta/kolasu/model/ModelTest.kt
+++ core/src/test/kotlin/com/strumenta/kolasu/model/ModelTest.kt
@@ -1,9 +1,18 @@
 package com.strumenta.kolasu.model
 
+import com.strumenta.kolasu.serialization.JsonGenerator
+import com.strumenta.kolasu.serialization.JsonSerializer
 import kotlin.test.assertEquals
 import org.junit.Test as test
 
 class MyNode(override val name: String) : Node(), Named
+
+data class NodeOverridingName(
+    override var name: String
+) : Node(), Named
+
+open class BaseNode(open var attr1: Int) : Node()
+data class ExtNode(override var attr1: Int) : BaseNode(attr1)
 
 class ModelTest {
 
@@ -40,4 +49,28 @@ class ModelTest {
         assertEquals(false, ref.tryToResolve(listOf(MyNode("foO"))))
         assertEquals(false, ref.resolved)
     }
+
+    @test
+    fun duplicatePropertiesInheritedByInterface() {
+        val properties = NodeOverridingName::class.nodeProperties
+        assertEquals(1, properties.size)
+        val json = JsonGenerator().generateString(NodeOverridingName("foo"))
+        assertEquals("""{
+  "#type": "com.strumenta.kolasu.model.NodeOverridingName",
+  "name": "foo"
+}""", json)
+    }
+
+
+    @test
+    fun duplicatePropertiesInheritedByClass() {
+        val properties = ExtNode::class.nodeProperties
+        assertEquals(1, properties.size)
+        val json = JsonGenerator().generateString(ExtNode(123))
+        assertEquals("""{
+  "#type": "com.strumenta.kolasu.model.ExtNode",
+  "attr1": 123
+}""", json)
+    }
+
 }
