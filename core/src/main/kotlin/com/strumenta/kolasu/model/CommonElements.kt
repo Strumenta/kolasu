c8fca50d60e57cf690870945f22a22b45b5420ce
Federico Tomassetti <federico@tomassetti.me>
2022-07-28 14:01:05 +0200

    introducing Common Elements

---
+++ core/src/main/kotlin/com/strumenta/kolasu/model/CommonElements.kt
@@ -0,0 +1,13 @@
+package com.strumenta.kolasu.model
+
+@NodeType
+interface Statement
+
+@NodeType
+interface Expression
+
+/**
+ * This should be used for definitions of classes, interfaces, structures
+ */
+@NodeType
+interface EntityDeclaration
