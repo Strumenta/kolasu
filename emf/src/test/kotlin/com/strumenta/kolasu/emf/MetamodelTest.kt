5dbd9cae289950d29c8e5165f9d757f4a2e77165
Federico Tomassetti <federico@tomassetti.me>
2022-08-02 08:40:13 +0200

    revise generateSimpleMetamodel test

--- emf/src/test/kotlin/com/strumenta/kolasu/emf/MetamodelTest.kt
+++ emf/src/test/kotlin/com/strumenta/kolasu/emf/MetamodelTest.kt
@@ -2,6 +2,7 @@ package com.strumenta.kolasu.emf
 
 import com.strumenta.kolasu.model.Node
 import com.strumenta.kolasu.model.Position
+import org.eclipse.emf.ecore.EClass
 import org.junit.Test
 import java.io.File
 import java.time.LocalDateTime
@@ -20,34 +21,41 @@ data class CompilationUnit(val statements: List<Statement>?, override var specif
 
 class MetamodelTest {
 
+    private fun temporaryFile(suffix: String) : File {
+        val f = kotlin.io.path.createTempFile(suffix = suffix).toFile()
+        f.deleteOnExit()
+        return f
+    }
+
     @Test
     fun generateSimpleMetamodel() {
         val metamodelBuilder = MetamodelBuilder("SimpleMM", "https://strumenta.com/simplemm", "simplemm")
         metamodelBuilder.provideClass(CompilationUnit::class)
         val ePackage = metamodelBuilder.generate()
-        ePackage.saveEcore(File("simplemm.ecore"))
-        ePackage.saveAsJson(File("simplemm.json"))
+        ePackage.saveEcore(temporaryFile("simplemm.ecore"))
+        ePackage.saveAsJson(temporaryFile("simplemm.json"))
         assertEquals("SimpleMM", ePackage.name)
         assertEquals(7, ePackage.eClassifiers.size)
 
-//        val cu: EClass = ePackage.eClassifiers.find { it.name == "CompilationUnit" } as EClass
-//        assertEquals(1, cu.eAllSuperTypes.size)
-//        assertEquals(0, cu.eAllAttributes.size)
-//        assertEquals(2, cu.eAllContainments.size)
-//        assertEquals(1, cu.eAllReferences.size)
-//        assertEquals(1, cu.eAllStructuralFeatures.size)
-//
-//        val e: EClass = ePackage.eClassifiers.find { it.name == "Expression" } as EClass
-//        assertEquals(true, e.isAbstract)
-//
-//        val sl: EClass = ePackage.eClassifiers.find { it.name == "StringLiteral" } as EClass
-//        assertEquals(1, sl.eAllSuperTypes.size)
-//        assertEquals(1, sl.eAllAttributes.size)
-//        assertEquals(0, sl.eAllContainments.size)
-//        assertEquals(0, sl.eAllReferences.size)
-//        assertEquals(1, sl.eAllStructuralFeatures.size)
-//
-//        val vd: EClass = ePackage.eClassifiers.find { it.name == "VarDeclaration" } as EClass
-//        assertEquals(2, vd.eAllAttributes.size)
+        val cu: EClass = ePackage.eClassifiers.find { it.name == "CompilationUnit" } as EClass
+        assertEquals(1, cu.eAllSuperTypes.size)
+        assertEquals(0, cu.eAllAttributes.size)
+        assertEquals(2, cu.eAllContainments.size)
+        assertEquals(2, cu.eAllReferences.size, cu.eAllReferences.joinToString(", ") { it.name })
+        assertEquals(2, cu.eAllStructuralFeatures.size)
+
+        val e: EClass = ePackage.eClassifiers.find { it.name == "Expression" } as EClass
+        assertEquals(true, e.isAbstract)
+
+        val sl: EClass = ePackage.eClassifiers.find { it.name == "StringLiteral" } as EClass
+        assertEquals(2, sl.eAllSuperTypes.size,
+            sl.eAllSuperTypes.joinToString(", ") { it.name })
+        assertEquals(1, sl.eAllAttributes.size)
+        assertEquals(1, sl.eAllContainments.size, sl.eAllContainments.joinToString(", ") { it.name })
+        assertEquals(1, sl.eAllReferences.size, sl.eAllReferences.joinToString(", ") { it.name })
+        assertEquals(2, sl.eAllStructuralFeatures.size)
+
+        val vd: EClass = ePackage.eClassifiers.find { it.name == "VarDeclaration" } as EClass
+        assertEquals(2, vd.eAllAttributes.size)
     }
 }
