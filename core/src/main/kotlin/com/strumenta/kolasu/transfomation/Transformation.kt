0fb2a5af08c533b20aa8483c5a593763982340c9
Federico Tomassetti <federico@tomassetti.me>
2022-09-05 16:55:30 +0200

    simplify transformation.kt

--- core/src/main/kotlin/com/strumenta/kolasu/transfomation/Transformation.kt
+++ core/src/main/kotlin/com/strumenta/kolasu/transfomation/Transformation.kt
@@ -270,35 +270,3 @@ open class ASTTransformer(
         return issue
     }
 }
-
-/**
- * A generic AST node. We use it to represent parts of a source tree that we don't know how to translate yet.
- */
-class GenericNode(parent: Node? = null) : Node() {
-    init {
-        this.parent = parent
-    }
-}
-
-/**
- * An AST node that marks the presence of an error, for example a syntactic or semantic error in the original tree.
- */
-interface ErrorNode {
-    val message: String
-    val position: Position?
-}
-
-/**
- * Generic implementation of [ErrorNode].
- */
-class GenericErrorNode(error: Exception? = null, message: String? = null) : Node(), ErrorNode {
-    override val message: String = message ?: error?.message ?: "Unspecified error node"
-}
-
-fun Node.errors(): Sequence<ErrorNode> = this.walkDescendants(ErrorNode::class)
-
-fun Node.findError(): ErrorNode? = this.errors().firstOrNull()
-
-fun Node.findGenericNode(): GenericNode? {
-    return if (this is GenericNode) this else this.children.firstNotNullOfOrNull { it.findGenericNode() }
-}
