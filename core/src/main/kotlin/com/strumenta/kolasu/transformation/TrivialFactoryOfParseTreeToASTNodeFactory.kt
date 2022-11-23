dcc68007e6b6431a64ffd2f972e78d670a79539a
Federico Tomassetti <federico@tomassetti.me>
2022-11-23 11:46:50 +0100

    supporting references by name

--- core/src/main/kotlin/com/strumenta/kolasu/transformation/TrivialFactoryOfParseTreeToASTNodeFactory.kt
+++ core/src/main/kotlin/com/strumenta/kolasu/transformation/TrivialFactoryOfParseTreeToASTNodeFactory.kt
@@ -1,20 +1,39 @@
 package com.strumenta.kolasu.transformation
 
 import com.strumenta.kolasu.model.Node
+import com.strumenta.kolasu.model.PossiblyNamed
+import com.strumenta.kolasu.model.ReferenceByName
+import org.antlr.v4.runtime.ParserRuleContext
 import org.antlr.v4.runtime.RuleContext
+import org.antlr.v4.runtime.Token
+import kotlin.reflect.KClass
+import kotlin.reflect.KClassifier
+import kotlin.reflect.KType
 import kotlin.reflect.full.memberProperties
 
 object TrivialFactoryOfParseTreeToASTNodeFactory {
 
-    private fun convert(value: Any, astTransformer: ASTTransformer) : Any? {
-        TODO()
+    public fun convert(value: Any?, astTransformer: ASTTransformer, expectedType: KType) : Any? {
+        if (value is Token) {
+            println(expectedType)
+            if (expectedType.classifier == ReferenceByName::class) {
+                return ReferenceByName<PossiblyNamed>(name = value.text)
+            }
+            return value.text
+        } else if (value is List<*>) {
+            return value.map { convert(it, astTransformer, expectedType.arguments[0].type!!) }
+            //TODO("type ${expectedType} ${expectedType.javaClass}")
+        } else if (value is ParserRuleContext) {
+            return astTransformer.transform(value)
+        }
+        TODO("value ${value} (${value?.javaClass})")
     }
 
-    inline fun <S : RuleContext, reified T : Node> trivialFactory(): (S, ASTTransformer) -> T? {
+    inline fun <S : RuleContext, reified T : Node> trivialFactory(vararg nameConversions: Pair<String, String>): (S, ASTTransformer) -> T? {
         return { parseTreeNode, astTransformer ->
             val constructors = T::class.constructors
-            println("class ${T::class}")
-            println("constructors ${constructors}")
+            //println("class ${T::class}")
+            //println("constructors ${constructors}")
             if (constructors.size != 1) {
                 throw java.lang.RuntimeException(
                     "Trivial Factory supports only classes with exactly one constructor. " +
@@ -23,16 +42,17 @@ object TrivialFactoryOfParseTreeToASTNodeFactory {
             }
             val constructor = constructors.first()
             val args: Array<Any?> = constructor.parameters.map {
-                println("looking for value for constructor parameter ${it} ${it.name}")
+                //println("looking for value for constructor parameter ${it} ${it.name}")
                 val parameterName = it.name
-                println("parseTreeClass ${parseTreeNode.javaClass.kotlin}")
-                val parseTreeMember = parseTreeNode.javaClass.kotlin.memberProperties.find { it.name == parameterName }
+                val searchedName = nameConversions.find { it.second == parameterName }?.let { it.first } ?: parameterName
+                //println("parseTreeClass ${parseTreeNode.javaClass.kotlin}")
+                val parseTreeMember = parseTreeNode.javaClass.kotlin.memberProperties.find { it.name == searchedName }
                 if (parseTreeMember == null) {
-                    TODO()
+                    TODO("Unable to convert ${parameterName} (looking for ${searchedName})")
                 } else {
                     val value = parseTreeMember.get(parseTreeNode)
-                    println("producing value $value (${value?.javaClass}) for $parameterName")
-                    convert(value, astTransformer)
+                    //println("producing value $value (${value?.javaClass}) for $parameterName")
+                    convert(value, astTransformer, it.type)
                 }
 
             }.toTypedArray()
--- core/src/test/kotlin/com/strumenta/kolasu/transformation/ParseTreeToASTTransformerTest.kt
+++ core/src/test/kotlin/com/strumenta/kolasu/transformation/ParseTreeToASTTransformerTest.kt
@@ -3,10 +3,16 @@ package com.strumenta.kolasu.transformation
 import com.strumenta.kolasu.mapping.ParseTreeToASTTransformer
 import com.strumenta.kolasu.model.Named
 import com.strumenta.kolasu.model.Node
+import com.strumenta.kolasu.model.ReferenceByName
 import com.strumenta.kolasu.testing.assertASTsAreEqual
 import com.strumenta.simplelang.AntlrEntityLexer
 import com.strumenta.simplelang.AntlrEntityParser
+import com.strumenta.simplelang.AntlrEntityParser.Boolean_typeContext
+import com.strumenta.simplelang.AntlrEntityParser.EntityContext
+import com.strumenta.simplelang.AntlrEntityParser.Entity_typeContext
+import com.strumenta.simplelang.AntlrEntityParser.FeatureContext
 import com.strumenta.simplelang.AntlrEntityParser.ModuleContext
+import com.strumenta.simplelang.AntlrEntityParser.String_typeContext
 import org.antlr.v4.runtime.*
 import org.antlr.v4.runtime.atn.ATNConfigSet
 import org.antlr.v4.runtime.dfa.DFA
@@ -14,7 +20,14 @@ import org.junit.Test
 import java.util.*
 
 data class EModule(override val name: String, val entities: MutableList<EEntity>) : Node(), Named
-data class EEntity(override val name: String) : Node(), Named
+data class EEntity(override val name: String, val features: MutableList<EFeature>) : Node(), Named
+
+data class EFeature(override val name: String, val type: EType) : Node(), Named
+
+sealed class EType: Node()
+class EStringType : EType()
+class EBooleanType : EType()
+data class EEntityRefType(val entity: ReferenceByName<EEntity>) : EType()
 
 class ParseTreeToASTTransformerTest {
 
@@ -23,12 +36,12 @@ class ParseTreeToASTTransformerTest {
             override fun syntaxError(
                 p0: Recognizer<*, *>?,
                 p1: Any?,
-                p2: Int,
-                p3: Int,
-                p4: String?,
+                line: Int,
+                column: Int,
+                message: String?,
                 p5: RecognitionException?
             ) {
-                throw RuntimeException()
+                throw RuntimeException("L${line}:${column}: ${message}")
             }
 
             override fun reportAmbiguity(
@@ -69,21 +82,21 @@ class ParseTreeToASTTransformerTest {
     }
 
     @Test
-    fun testEntitiesTransformer() {
+    fun testSimpleEntitiesTransformer() {
         val transformer = ParseTreeToASTTransformer(allowGenericNode = false)
         transformer.registerNodeFactory(
             ModuleContext::class,
             TrivialFactoryOfParseTreeToASTNodeFactory.trivialFactory<ModuleContext, EModule>()
         )
-//        transformer.registerNodeFactory(ModuleContext::class) { parseTree, ASTTransformer ->
-//            EModule("Foo", mutableListOf())
-//        }
-
+        transformer.registerNodeFactory(
+            EntityContext::class,
+            TrivialFactoryOfParseTreeToASTNodeFactory.trivialFactory<EntityContext, EEntity>()
+        )
         val expectedAST = EModule(
             "M",
             mutableListOf(
-                EEntity("FOO"),
-                EEntity("BAR"),
+                EEntity("FOO", mutableListOf()),
+                EEntity("BAR", mutableListOf())
             )
         )
         val actualAST = transformer.transform(
@@ -92,6 +105,64 @@ class ParseTreeToASTTransformerTest {
             module M {
                entity FOO { }
                entity BAR { }
+            }
+                """.trimIndent()
+            )
+        )!!
+        assertASTsAreEqual(expectedAST, actualAST)
+    }
+
+    @Test
+    fun testEntitiesWithFeaturesTransformer() {
+        val transformer = ParseTreeToASTTransformer(allowGenericNode = false)
+        transformer.registerNodeFactory(
+            ModuleContext::class,
+            TrivialFactoryOfParseTreeToASTNodeFactory.trivialFactory<ModuleContext, EModule>()
+        )
+        transformer.registerNodeFactory(
+            EntityContext::class,
+            TrivialFactoryOfParseTreeToASTNodeFactory.trivialFactory<EntityContext, EEntity>()
+        )
+        transformer.registerNodeFactory(
+            FeatureContext::class,
+            TrivialFactoryOfParseTreeToASTNodeFactory.trivialFactory<FeatureContext, EFeature>()
+        )
+        transformer.registerNodeFactory(
+            String_typeContext::class,
+            TrivialFactoryOfParseTreeToASTNodeFactory.trivialFactory<String_typeContext, EStringType>()
+        )
+        transformer.registerNodeFactory(
+            Boolean_typeContext::class,
+            TrivialFactoryOfParseTreeToASTNodeFactory.trivialFactory<Boolean_typeContext, EBooleanType>()
+        )
+        transformer.registerNodeFactory(
+            Entity_typeContext::class,
+            TrivialFactoryOfParseTreeToASTNodeFactory.trivialFactory<Entity_typeContext, EEntityRefType>("target" to "entity")
+        )
+        val expectedAST = EModule(
+            "M",
+            mutableListOf(
+                EEntity("FOO", mutableListOf(
+                    EFeature("A", EStringType()),
+                    EFeature("B", EBooleanType()),
+                )),
+                EEntity("BAR", mutableListOf(
+                    EFeature("C", EEntityRefType(ReferenceByName("FOO")))
+                )),
+            )
+        )
+        val actualAST = transformer.transform(
+            parseEntities(
+                """
+            module M {
+               entity FOO {
+                A: string;
+                B: boolean;
+                 
+               }
+               entity BAR {
+                 C: FOO;
+               }
             }
                 """.trimIndent()
             )
