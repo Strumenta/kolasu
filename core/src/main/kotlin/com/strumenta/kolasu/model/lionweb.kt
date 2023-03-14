ce928f6a6f59290a16ae110d5fe5bc2f80ecce4b
Federico Tomassetti <federico@strumenta.com>
2023-03-14 13:39:15 +0100

    work on supporting enums

--- core/src/main/kotlin/com/strumenta/kolasu/model/lionweb.kt
+++ core/src/main/kotlin/com/strumenta/kolasu/model/lionweb.kt
@@ -9,6 +9,7 @@ import org.lionweb.lioncore.java.metamodel.Property
 import kotlin.reflect.KClass
 import kotlin.reflect.KProperty1
 import kotlin.reflect.KType
+import kotlin.reflect.full.allSuperclasses
 import kotlin.reflect.full.createType
 import kotlin.reflect.full.superclasses
 
@@ -18,64 +19,73 @@ val <A:ASTNode>KClass<A>.concept : Concept
     get() = conceptsMemory.getOrPut(this) { calculateConcept(this) }
 
 fun <A:ASTNode>calculateConcept(kClass: KClass<A>) : Concept {
-    val concept = Concept()
-    concept.simpleName = kClass.simpleName
+    try {
+        val concept = Concept()
+        concept.simpleName = kClass.simpleName
 
-    val superclasses = kClass.superclasses.toMutableList()
-    //require(superclasses.contains(ASTNode::class))
-    superclasses.remove(ASTNode::class)
-    superclasses.forEach { superclass ->
-        if (superclass.java.isInterface) {
-            // TODO consider
-        } else {
-            concept.extendedConcept = (superclass as KClass<out ASTNode>).concept
-        }
-    }
-    if (concept.extendedConcept == null) {
-        concept.extendedConcept = StarLasuMetamodel.astNode
-    }
-
-    val metamodelQName = kClass.qualifiedName!!.removeSuffix(".${kClass.simpleName}") + ".Metamodel"
-    val metamodelKClass = kClass.java.classLoader.loadClass(metamodelQName).kotlin
-    val metamodelInstance = metamodelKClass.objectInstance as Metamodel
-    metamodelInstance.addElement(concept)
-
-    kClass.nodeProperties.forEach {
-        val provideNodes = PropertyDescription.providesNodes(it as KProperty1<in ASTNode, *>)
-        val ref = (it as KProperty1<in ASTNode, *>).isReference
-        when {
-            !provideNodes -> {
-                val property = Property()
-                property.simpleName = it.name
-                when (it.returnType) {
-                    Boolean::class.createType() -> {
-                        property.type = LionCoreBuiltins.getBoolean()
-                    }
-                    String::class.createType() -> {
-                        property.type = LionCoreBuiltins.getString()
-                    }
-                    Int::class.createType() -> {
-                        property.type = LionCoreBuiltins.getInteger()
-                    }
-                    else -> {
-                        TODO("Return type: ${it.returnType}")
-                    }
-                }
-                concept.addFeature(property)
-            }
-            provideNodes && ref -> TODO()
-            provideNodes && !ref -> {
-                val containment = Containment()
-                containment.simpleName = it.name
-                if ((it.returnType.classifier as KClass<*>) == List::class) {
-                    containment.isMultiple = true
-                    containment.type = (it.returnType.arguments[0].type!!.classifier as KClass<out ASTNode>).concept
-                } else {
-                    containment.type = (it.returnType.classifier as KClass<out ASTNode>).concept
-                }
-                concept.addFeature(containment)
+        val superclasses = kClass.superclasses.toMutableList()
+        //require(superclasses.contains(ASTNode::class))
+        superclasses.remove(ASTNode::class)
+        superclasses.forEach { superclass ->
+            if (superclass.java.isInterface) {
+                // TODO consider
+            } else {
+                concept.extendedConcept = (superclass as KClass<out ASTNode>).concept
             }
         }
+        if (concept.extendedConcept == null) {
+            concept.extendedConcept = StarLasuMetamodel.astNode
+        }
+
+        val metamodelQName = kClass.qualifiedName!!.removeSuffix(".${kClass.simpleName}") + ".Metamodel"
+        val metamodelKClass = kClass.java.classLoader.loadClass(metamodelQName).kotlin
+        val metamodelInstance = metamodelKClass.objectInstance as Metamodel
+        metamodelInstance.addElement(concept)
+
+        kClass.nodeProperties.forEach {
+            val provideNodes = PropertyDescription.providesNodes(it as KProperty1<in ASTNode, *>)
+            val ref = (it as KProperty1<in ASTNode, *>).isReference
+            when {
+                !provideNodes -> {
+                    val property = Property()
+                    property.simpleName = it.name
+                    when (it.returnType.classifier) {
+                        Boolean::class -> {
+                            property.type = LionCoreBuiltins.getBoolean()
+                        }
+                        String::class -> {
+                            property.type = LionCoreBuiltins.getString()
+                        }
+                        Int::class -> {
+                            property.type = LionCoreBuiltins.getInteger()
+                        }
+                        else -> {
+                            if ((it.returnType.classifier as? KClass<*>)?.allSuperclasses?.contains(Enum::class) ?: false) {
+                                // TODO add support for enums
+                                property.type = LionCoreBuiltins.getString()
+                            } else {
+                                TODO("Return type: ${it.returnType.classifier} (${it.returnType.classifier?.javaClass}")
+                            }
+                        }
+                    }
+                    concept.addFeature(property)
+                }
+                provideNodes && ref -> TODO()
+                provideNodes && !ref -> {
+                    val containment = Containment()
+                    containment.simpleName = it.name
+                    if ((it.returnType.classifier as KClass<*>) == List::class) {
+                        containment.isMultiple = true
+                        containment.type = (it.returnType.arguments[0].type!!.classifier as KClass<out ASTNode>).concept
+                    } else {
+                        containment.type = (it.returnType.classifier as KClass<out ASTNode>).concept
+                    }
+                    concept.addFeature(containment)
+                }
+            }
+        }
+        return concept
+    } catch (e: Throwable) {
+        throw RuntimeException("Unable to calculate concept for $kClass", e)
     }
-    return concept
 }
