4e0bf7bf14f2547f29feb9ecf8a5bfdaf40aed94
Federico Tomassetti <federico@strumenta.com>
2023-03-17 13:49:08 +0100

    make LocalSymbolResolver accept Node

--- core/src/main/kotlin/com/strumenta/kolasu/symbolresolution/LocalSymbolResolver.kt
+++ core/src/main/kotlin/com/strumenta/kolasu/symbolresolution/LocalSymbolResolver.kt
@@ -7,10 +7,10 @@ import com.strumenta.kolasu.validation.Issue
  * This object performs symbol resolution within a single AST.
  * It is not to be used to perform cross-ASTs symbol resolution.
  */
-abstract class LocalSymbolResolver<T : Node> {
+abstract class LocalSymbolResolver {
     /**
      * This will resolve symbols on the given AST. It will set the links in the ReferenceByName found.
      * It will return a list of issues encountered during symbol resolution.
      */
-    abstract fun resolveSymbols(root: T) : List<Issue>
+    abstract fun resolveSymbols(root: Node) : List<Issue>
 }
