9b137da9b3f6d7c842b53df4a60434ab3a19a7a6
Federico Tomassetti <federico@strumenta.com>
2023-03-27 14:41:36 +0200

    Rename EMFEnabledParser to EcoreEnabledParser

--- emf/src/main/kotlin/com/strumenta/kolasu/emf/cli/EMFCLITool.kt
+++ emf/src/main/kotlin/com/strumenta/kolasu/emf/cli/EMFCLITool.kt
@@ -5,15 +5,15 @@ import com.github.ajalt.clikt.core.context
 import com.github.ajalt.clikt.core.subcommands
 import com.github.ajalt.clikt.output.CliktConsole
 import com.strumenta.kolasu.cli.ParserInstantiator
-import com.strumenta.kolasu.emf.EMFEnabledParser
 import com.strumenta.kolasu.emf.EMFMetamodelSupport
+import com.strumenta.kolasu.emf.EcoreEnabledParser
 import com.strumenta.kolasu.model.Node
 
 class EMFCLITool<R : Node, P>(
     parserInstantiator: ParserInstantiator<P>,
     metamodelSupport: EMFMetamodelSupport,
     replacedConsole: CliktConsole? = null
-) : CliktCommand(invokeWithoutSubcommand = false) where P : EMFEnabledParser<R, *, *> {
+) : CliktCommand(invokeWithoutSubcommand = false) where P : EcoreEnabledParser<R, *, *> {
     init {
         subcommands(EMFModelCommand(parserInstantiator), EMFMetaModelCommand(metamodelSupport))
         context { replacedConsole?.apply { console = replacedConsole } }
