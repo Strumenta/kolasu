fd98744d72a741f016260f5e028cea6f0acdf51e
Federico Tomassetti <federico@strumenta.com>
2023-04-27 18:06:59 +0200

    validate StarLasu metamodel

--- core/src/main/kotlin/com/strumenta/kolasu/metamodel/StarLasuMetamodel.kt
+++ core/src/main/kotlin/com/strumenta/kolasu/metamodel/StarLasuMetamodel.kt
@@ -5,10 +5,12 @@ import com.strumenta.kolasu.model.lionweb.recordConceptForClass
 import com.strumenta.kolasu.model.lionweb.recordConceptInterfaceForClass
 import org.lionweb.lioncore.java.metamodel.Concept
 import org.lionweb.lioncore.java.metamodel.ConceptInterface
+import org.lionweb.lioncore.java.metamodel.FeaturesContainer
 import org.lionweb.lioncore.java.metamodel.LionCoreBuiltins
 import org.lionweb.lioncore.java.metamodel.Metamodel
 import org.lionweb.lioncore.java.metamodel.PrimitiveType
 import org.lionweb.lioncore.java.metamodel.Property
+import org.lionweb.lioncore.java.utils.MetamodelValidator
 
 object StarLasuMetamodel : Metamodel() {
     val astNode: Concept
@@ -18,6 +20,11 @@ object StarLasuMetamodel : Metamodel() {
     val position: PrimitiveType
     val char: PrimitiveType
     init {
+        this.name = "com.strumenta.StarLasu"
+        this.id = this.name.replace('.', '_')
+        this.key = this.id
+        this.version = "1"
+
         astNode = Concept(this, "ASTNode", "StarLasu-ASTNode")
         genericErrorNode = Concept(this, "GenericErrorNode", "StarLasu-GenericErrorNode")
         named = ConceptInterface(this, "Named", "StarLasu-Named")
@@ -32,5 +39,20 @@ object StarLasuMetamodel : Metamodel() {
         recordConceptForClass(GenericErrorNode::class.java, genericErrorNode)
         recordConceptInterfaceForClass(Named::class.java, named)
         recordConceptInterfaceForClass(PossiblyNamed::class.java, possiblyNamed)
+
+        elements.forEach { me ->
+            me.key = me.id
+            if (me is FeaturesContainer<*>) {
+                me.features.forEach { f ->
+                    f.id = me.id + "-" + f.name
+                    f.key = f.id
+                }
+            }
+        }
+
+        val result = MetamodelValidator().validate(this)
+        if (!result.isSuccessful) {
+            throw RuntimeException("The StarLasu Metamodel is not valid: $result")
+        }
     }
 }
