e7322ec6fd7d35983a7d3962fbc2096df4259db5
Federico Tomassetti <federico@strumenta.com>
2023-05-02 15:36:13 +0200

    converting to kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,12 @@
+pluginManagement {
+    val kotlin_version: String by settings
+    val dokka_version: String by settings
+    plugins {
+        id("org.jetbrains.kotlin.jvm") version "$kotlin_version"
+        id("org.jetbrains.dokka") version "$dokka_version"
+    }
+}
+
 rootProject.name = "kolasu"
 include("core")
 include("emf")
