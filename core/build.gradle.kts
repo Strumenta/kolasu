e7bfddd658fd2ea65084639569f9f4094444d228
Federico Tomassetti <federico@strumenta.com>
2023-05-05 14:35:42 +0200

    migrate the core module to gradle kts

---
+++ core/build.gradle.kts
@@ -0,0 +1,18 @@
+val clikt_version = extra["clikt_version"]
+
+dependencies {
+    implementation("com.github.salomonbrys.kotson:kotson:2.5.0")
+    implementation("org.redundent:kotlin-xml-builder:1.9.0")
+    implementation("com.github.doyaaaaaken:kotlin-csv-jvm:1.8.0")
+
+    implementation("com.github.ajalt.clikt:clikt:$clikt_version")
+}
+
+publishing {
+    addSonatypeRepo(project)
+    addPublication("kolasu_core", "Framework to work with AST and building languages", project)
+}
+
+signing {
+    sign(publishing.publications["kolasu_core"])
+}
