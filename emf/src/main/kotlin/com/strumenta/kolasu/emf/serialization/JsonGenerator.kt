70a13977cce618168fa6da208b2f8d01de57ae9a
Federico Tomassetti <federico@tomassetti.me>
2023-05-07 09:31:37 +0200

    formatting

--- emf/src/main/kotlin/com/strumenta/kolasu/emf/serialization/JsonGenerator.kt
+++ emf/src/main/kotlin/com/strumenta/kolasu/emf/serialization/JsonGenerator.kt
@@ -1,7 +1,6 @@
 package com.strumenta.kolasu.emf.serialization
 
 import com.strumenta.kolasu.emf.STARLASU_METAMODEL
-import com.strumenta.kolasu.emf.saveAsJson
 import com.strumenta.kolasu.emf.toEObject
 import com.strumenta.kolasu.model.Node
 import com.strumenta.kolasu.validation.Result
@@ -16,7 +15,7 @@ import java.io.ByteArrayOutputStream
 class JsonGenerator {
     fun generateEMFString(result: Result<out Node>, astPackage: EPackage): String {
         val uri: URI = URI.createFileURI("dummy-URI.json") ?: throw IllegalStateException("URI not created")
-        var resourceSet : ResourceSet = ResourceSetImpl()
+        var resourceSet: ResourceSet = ResourceSetImpl()
         resourceSet.resourceFactoryRegistry.contentTypeToFactoryMap["application/json"] = JsonResourceFactory()
         resourceSet.resourceFactoryRegistry.extensionToFactoryMap["json"] = JsonResourceFactory()
         val resource: Resource = resourceSet.createResource(uri)
