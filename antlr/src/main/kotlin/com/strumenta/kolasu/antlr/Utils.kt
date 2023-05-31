28da2d67bf57f7427fbdc4bf4cf56f813b1f2d29
Federico Tomassetti <federico@strumenta.com>
2023-05-31 10:59:41 +0200

    reorganize Node.detachFromParseTree

--- antlr/src/main/kotlin/com/strumenta/kolasu/antlr/Utils.kt
+++ antlr/src/main/kotlin/com/strumenta/kolasu/antlr/Utils.kt
@@ -1,13 +1,15 @@
 package com.strumenta.kolasu.antlr
 
+import com.strumenta.kolasu.antlr.parsing.ParseTreeOrigin
 import com.strumenta.kolasu.model.Node
 import com.strumenta.kolasu.model.SimpleOrigin
-import com.strumenta.kolasu.model.contains
-import com.strumenta.kolasu.model.minusAssign
 
+/**
+ * Remove links to the ParseTree, in order to save memory.
+ */
 fun Node.detachFromParseTree(keepRange: Boolean = true, keepSourceText: Boolean = false) {
     val existingOrigin = origin
-    if (existingOrigin != null) {
+    if (existingOrigin is ParseTreeOrigin) {
         if (keepRange || keepSourceText) {
             this.origin = SimpleOrigin(
                 if (keepRange) existingOrigin.range else null,
@@ -15,9 +17,6 @@ fun Node.detachFromParseTree(keepRange: Boolean = true, keepSourceText: Boolean 
             )
         } else {
             this.origin = null
-        }
-        if ((existingOrigin is Node) && (this in existingOrigin.destinations)) {
-            existingOrigin.destinations -= this
         }
     }
 }
