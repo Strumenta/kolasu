591d026196f713aa10a11c76ebed6dd2a0bdf685
Federico Tomassetti <federico@strumenta.com>
2023-06-01 17:35:17 +0200

    wip

--- core/src/main/kotlin/com/strumenta/kolasu/model/observable/Observer.kt
+++ core/src/main/kotlin/com/strumenta/kolasu/model/observable/Observer.kt
@@ -1,9 +1,17 @@
 package com.strumenta.kolasu.model.observable
 
 import com.strumenta.kolasu.model.Node
+import org.reactivestreams.Subscriber
 
-interface Observer<N : Node> {
-    fun receivePropertyChangeNotification(node: N, propertyName: String, oldValue: Any?, newValue: Any?) {}
+sealed class NodeNotification<N: Node> {
+    abstract val node: N
+}
+data class PropertyChangeNodeNotification<N: Node>(override val node: N) : NodeNotification<N>()
+
+interface Observer<N : Node> : Subscriber<NodeNotification<N>> {
+    fun receivePropertyChangeNotification(node: N, propertyName: String, oldValue: Any?, newValue: Any?) {
+        onNext(PropertyChangeNodeNotification(node))
+    }
     fun receivePropertyAddedNotification(node: N, propertyName: String, added: Any?) {}
     fun receivePropertyRemovedNotification(node: N, propertyName: String, removed: Any?) {}
 }
