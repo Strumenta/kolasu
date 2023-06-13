6ada8e64ad28f6c105c3b0273dbb099d358d15ee
Federico Tomassetti <federico@strumenta.com>
2023-06-13 08:45:57 +0200

    correcting compilation issue

--- lionweb/src/main/kotlin/com/strumenta/kolasu/lionweb/LionWebModelImporterAndExporter.kt
+++ lionweb/src/main/kotlin/com/strumenta/kolasu/lionweb/LionWebModelImporterAndExporter.kt
@@ -28,7 +28,7 @@ private val com.strumenta.kolasu.model.Node.positionalID: String
             "root"
         } else {
             val cp = this.containingProperty()!!
-            val postfix = if (cp.multiple) "${cp.name}[${this.indexInContainingProperty()!!}]" else cp.name
+            val postfix = if (cp.isMultiple) "${cp.name}[${this.indexInContainingProperty()!!}]" else cp.name
             "${this.parent!!.positionalID}-$postfix"
         }
     }
