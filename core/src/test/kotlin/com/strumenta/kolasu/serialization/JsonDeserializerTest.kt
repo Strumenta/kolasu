24807dacbf6516772cf004c9c94ed13014d98be2
Gabriele Tomassetti <gabriele@strumenta.com>
2023-08-15 09:11:07 +0200

    Add deserialization tests for checking JSON Generation with streaming

--- core/src/test/kotlin/com/strumenta/kolasu/serialization/JsonDeserializerTest.kt
+++ core/src/test/kotlin/com/strumenta/kolasu/serialization/JsonDeserializerTest.kt
@@ -1,11 +1,13 @@
 package com.strumenta.kolasu.serialization
 
+import com.google.gson.stream.JsonWriter
 import com.strumenta.kolasu.model.Point
 import com.strumenta.kolasu.model.Position
 import com.strumenta.kolasu.validation.Issue
 import com.strumenta.kolasu.validation.IssueType
 import com.strumenta.kolasu.validation.Result
 import org.junit.Test
+import java.io.StringWriter
 import kotlin.test.assertEquals
 
 class JsonDeserializerTest {
@@ -58,6 +60,82 @@ class JsonDeserializerTest {
             null,
         )
         val json = JsonGenerator().generateString(originalResult)
+        val deserialized: Result<MyRoot> = JsonDeserializer().deserializeResult(MyRoot::class.java, json)
+        assertEquals(originalResult, deserialized)
+    }
+
+    @Test
+    fun deserializeNodeFromStreaming() {
+        val myRoot = MyRoot(
+            mainSection = Section(
+                "Section1",
+                listOf(
+                    Content(1, null),
+                    Content(2, Content(3, Content(4, null))),
+                ),
+            ),
+            otherSections = listOf(),
+        )
+        val writer = StringWriter()
+        JsonGenerator().generateJSONWithStreaming(root = myRoot, writer = JsonWriter(writer))
+        val json = writer.toString()
+        val deserialized = JsonDeserializer().deserialize(MyRoot::class.java, json)
+        assertEquals(myRoot, deserialized)
+    }
+
+    @Test
+    fun deserializeNodeFromStreamingWithShortNames() {
+        val myRoot = MyRoot(
+            mainSection = Section(
+                "Section1",
+                listOf(
+                    Content(1, null),
+                    Content(2, Content(3, Content(4, null))),
+                ),
+            ),
+            otherSections = listOf(),
+        )
+        val writer = StringWriter()
+        JsonGenerator().generateJSONWithStreaming(root = myRoot, writer = JsonWriter(writer), shortClassNames = true)
+        val json = writer.toString()
+        val deserialized = JsonDeserializer().deserialize(MyRoot::class.java, json)
+        assertEquals(myRoot, deserialized)
+    }
+
+    @Test
+    fun deserializeNegativeResultFromJsonStreaming() {
+        val originalResult: Result<MyRoot> = Result(
+            listOf(
+                Issue(
+                    IssueType.LEXICAL,
+                    "foo",
+                    position = Position(Point(1, 10), Point(4, 540)),
+                ),
+            ),
+            null,
+        )
+        val writer = StringWriter()
+        JsonGenerator().generateJSONWithStreaming(result = originalResult, writer = JsonWriter(writer), shortClassNames = false)
+        val json = writer.toString()
+        val deserialized: Result<MyRoot> = JsonDeserializer().deserializeResult(MyRoot::class.java, json)
+        assertEquals(originalResult, deserialized)
+    }
+
+    @Test
+    fun deserializeNegativeResultFromJsonStreamingWithShortNames() {
+        val originalResult: Result<MyRoot> = Result(
+            listOf(
+                Issue(
+                    IssueType.LEXICAL,
+                    "foo",
+                    position = Position(Point(1, 10), Point(4, 540)),
+                ),
+            ),
+            null,
+        )
+        val writer = StringWriter()
+        JsonGenerator().generateJSONWithStreaming(result = originalResult, writer = JsonWriter(writer), shortClassNames = true)
+        val json = writer.toString()
         val deserialized: Result<MyRoot> = JsonDeserializer().deserializeResult(MyRoot::class.java, json)
         assertEquals(originalResult, deserialized)
     }
