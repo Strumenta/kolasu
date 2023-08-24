bae5c7c6444efd1b2b0f0eaed705adc6e932f3b4
Alessio Stalla <alessiostalla@gmail.com>
2023-08-24 13:37:45 +0200

    Lint

--- core/src/test/kotlin/com/strumenta/kolasu/parsing/KolasuParserTest.kt
+++ core/src/test/kotlin/com/strumenta/kolasu/parsing/KolasuParserTest.kt
@@ -7,7 +7,6 @@ import com.strumenta.simplelang.SimpleLangLexer
 import com.strumenta.simplelang.SimpleLangParser
 import org.antlr.v4.runtime.CharStream
 import org.antlr.v4.runtime.Lexer
-import org.antlr.v4.runtime.Parser
 import org.antlr.v4.runtime.TokenStream
 import org.junit.Test
 import kotlin.test.assertEquals
