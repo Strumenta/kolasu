0d6e299f07c21c9aec974514189cbbd9cdc98c60
Lorenzo Addazi <lorenzo.addazi@strumenta.com>
2023-10-13 12:27:25 +0000

    Improved symbol resolution and type computation APIs
    Signed-off-by: Lorenzo Addazi <lorenzo.addazi@strumenta.com>

--- core/src/main/kotlin/com/strumenta/kolasu/utils/Memoization.kt
+++ core/src/main/kotlin/com/strumenta/kolasu/utils/Memoization.kt
@@ -8,3 +8,13 @@ class Memoize1<in T, out R>(val f: (T) -> R) : (T) -> R {
 }
 
 fun <T, R> ((T) -> R).memoize(): (T) -> R = Memoize1(this)
+
+class MemoizeExtension1<in S, in T, out R>(val f: (S, T) -> R) : (S, T) -> R {
+    private val values = mutableMapOf<T, R>()
+
+    override fun invoke(context: S, argument: T): R {
+        return values.getOrPut(argument) { f(context, argument) }
+    }
+}
+
+fun <S, T, R>((S, T) -> R).memoize(): (S, T) -> R = MemoizeExtension1(this)
