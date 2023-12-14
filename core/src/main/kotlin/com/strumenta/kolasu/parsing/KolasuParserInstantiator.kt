8c1af04012145454d8d4fc64da65bdd70377352f
Federico Tomassetti <federico@strumenta.com>
2023-12-14 17:40:34 +0100

    document KolasuParserInstantiator

---
+++ core/src/main/kotlin/com/strumenta/kolasu/parsing/KolasuParserInstantiator.kt
@@ -0,0 +1,11 @@
+package com.strumenta.kolasu.parsing
+
+import java.io.File
+
+/**
+ * This interface is used when we need to configure parsers. For example, it can be used for testing, when we need
+ * to instantiate a parser with a particular configuration (e.g., specifying include lists of files).
+ */
+interface KolasuParserInstantiator {
+    fun instantiate(fileToParse: File): KolasuParser<*, *, *, *>
+}
