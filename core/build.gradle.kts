f778d5b79b551e13c26c4672c253f3e5cb46c253
Federico Tomassetti <federico@tomassetti.me>
2023-12-16 16:29:05 +0100

    cleaning

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -1,12 +1,4 @@
-val clikt_version = extra["clikt_version"]
-val gson_version = extra["gson_version"]
-
 dependencies {
-    // implementation("org.redundent:kotlin-xml-builder:1.9.0")
-    // implementation("com.github.doyaaaaaken:kotlin-csv-jvm:1.8.0")
-    // implementation("com.google.code.gson:gson:$gson_version")
-
-    api("com.github.ajalt.clikt:clikt:$clikt_version")
     api("io.reactivex.rxjava3:rxjava:3.1.1")
 }
 
