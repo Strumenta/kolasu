bce5fa62b9679edff57a69a2e75c4c0aff3d4769
Federico Tomassetti <federico@tomassetti.me>
2024-01-27 16:46:22 +0100

    wip: making ast module multiplatform

--- ast/build.gradle.kts
+++ ast/build.gradle.kts
@@ -1,15 +1,33 @@
+plugins {
+    kotlin("multiplatform")
+    id("org.jlleitschuh.gradle.ktlint")
+    id("maven-publish")
+    id("signing")
+    id("org.jetbrains.dokka")
+    id("org.jetbrains.kotlinx.kover")
+}
+
 val cliktVersion = extra["clikt_version"]
 val gsonVersion = extra["gson_version"]
 
-dependencies {
-    api("com.badoo.reaktive:reaktive:2.0.1")
+kotlin {
+    jvm {
+        withJava()
+    }
+    sourceSets {
+        val jvmMain by getting {
+            dependencies {
+                api("com.badoo.reaktive:reaktive:2.0.1")
+            }
+        }
+    }
 }
 
 publishing {
     addSonatypeRepo(project)
-    addPublication("kolasu_ast", "Framework to work with AST and building languages", project)
+    // addPublication("kolasu_ast", "Framework to work with AST and building languages", project)
 }
 
 signing {
-    sign(publishing.publications["kolasu_ast"])
+//    sign(publishing.publications["kolasu_ast"])
 }
