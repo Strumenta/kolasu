380e48e4e2377e7d238041eb93c49172fefe603e
Federico Tomassetti <federico@strumenta.com>
2024-02-11 18:33:10 +0100

    wip

--- kotlin-ir-plugin/src/main/kotlin/com/strumenta/kolasu/kcp/StarLasuIrGenerationExtension.kt
+++ kotlin-ir-plugin/src/main/kotlin/com/strumenta/kolasu/kcp/StarLasuIrGenerationExtension.kt
@@ -1,4 +1,4 @@
-@file:OptIn(FirIncompatiblePluginAPI::class, ObsoleteDescriptorBasedAPI::class)
+@file:OptIn(FirIncompatiblePluginAPI::class, ObsoleteDescriptorBasedAPI::class, UnsafeDuringIrConstructionAPI::class)
 
 package com.strumenta.kolasu.kcp
 
@@ -7,67 +7,45 @@ import com.strumenta.kolasu.model.FeatureDescription
 import com.strumenta.kolasu.model.FeatureType
 import com.strumenta.kolasu.model.Multiplicity
 import com.strumenta.kolasu.model.Node
-import com.strumenta.kolasu.model.NodeLike
-import com.strumenta.kolasu.model.ReferenceByName
 import org.jetbrains.kotlin.backend.common.extensions.FirIncompatiblePluginAPI
 import org.jetbrains.kotlin.backend.common.extensions.IrGenerationExtension
 import org.jetbrains.kotlin.backend.common.extensions.IrPluginContext
 import org.jetbrains.kotlin.backend.common.lower.DeclarationIrBuilder
 import org.jetbrains.kotlin.backend.common.lower.irBlockBody
-import org.jetbrains.kotlin.backend.wasm.ir2wasm.allSuperInterfaces
 import org.jetbrains.kotlin.cli.common.messages.CompilerMessageSeverity
 import org.jetbrains.kotlin.cli.common.messages.MessageCollector
 import org.jetbrains.kotlin.descriptors.DescriptorVisibilities
-import org.jetbrains.kotlin.descriptors.DescriptorVisibility
 import org.jetbrains.kotlin.descriptors.Modality
 import org.jetbrains.kotlin.ir.ObsoleteDescriptorBasedAPI
 import org.jetbrains.kotlin.ir.UNDEFINED_OFFSET
-import org.jetbrains.kotlin.ir.backend.js.utils.realOverrideTarget
-import org.jetbrains.kotlin.ir.builders.declarations.addFunction
+import org.jetbrains.kotlin.ir.builders.declarations.buildFun
+import org.jetbrains.kotlin.ir.builders.irBlockBody
+import org.jetbrains.kotlin.ir.builders.irBoolean
 import org.jetbrains.kotlin.ir.builders.irCall
-import org.jetbrains.kotlin.ir.builders.irCallOp
+import org.jetbrains.kotlin.ir.builders.irCallConstructor
 import org.jetbrains.kotlin.ir.builders.irGet
-import org.jetbrains.kotlin.ir.builders.irLetS
-import org.jetbrains.kotlin.ir.builders.irNull
+import org.jetbrains.kotlin.ir.builders.irGetField
 import org.jetbrains.kotlin.ir.builders.irReturn
 import org.jetbrains.kotlin.ir.builders.irString
-import org.jetbrains.kotlin.ir.builders.irTemporary
+import org.jetbrains.kotlin.ir.builders.irVararg
 import org.jetbrains.kotlin.ir.declarations.IrClass
 import org.jetbrains.kotlin.ir.declarations.IrDeclarationOrigin
 import org.jetbrains.kotlin.ir.declarations.IrModuleFragment
-import org.jetbrains.kotlin.ir.declarations.IrSimpleFunction
 import org.jetbrains.kotlin.ir.declarations.impl.IrFactoryImpl
-import org.jetbrains.kotlin.ir.declarations.impl.IrFunctionImpl
-import org.jetbrains.kotlin.ir.declarations.impl.IrPropertyImpl
-import org.jetbrains.kotlin.ir.linkage.partial.PartiallyLinkedDeclarationOrigin
-import org.jetbrains.kotlin.ir.symbols.IrClassifierSymbol
-import org.jetbrains.kotlin.ir.symbols.IrPropertySymbol
-import org.jetbrains.kotlin.ir.symbols.impl.IrClassPublicSymbolImpl
-import org.jetbrains.kotlin.ir.symbols.impl.IrClassSymbolImpl
-import org.jetbrains.kotlin.ir.symbols.impl.IrPropertyPublicSymbolImpl
-import org.jetbrains.kotlin.ir.symbols.impl.IrPropertySymbolImpl
-import org.jetbrains.kotlin.ir.symbols.impl.IrSimpleFunctionPublicSymbolImpl
-import org.jetbrains.kotlin.ir.types.IrSimpleType
-import org.jetbrains.kotlin.ir.types.IrType
-import org.jetbrains.kotlin.ir.types.IrTypeArgument
-import org.jetbrains.kotlin.ir.types.SimpleTypeNullability
-import org.jetbrains.kotlin.ir.types.impl.IrSimpleTypeImpl
-import org.jetbrains.kotlin.ir.util.IdSignature
-import org.jetbrains.kotlin.ir.util.addArguments
-import org.jetbrains.kotlin.ir.util.addFakeOverrides
+import org.jetbrains.kotlin.ir.expressions.IrStatementOrigin
+import org.jetbrains.kotlin.ir.expressions.impl.IrFunctionExpressionImpl
+import org.jetbrains.kotlin.ir.symbols.UnsafeDuringIrConstructionAPI
+import org.jetbrains.kotlin.ir.types.typeWith
+import org.jetbrains.kotlin.ir.util.SYNTHETIC_OFFSET
+import org.jetbrains.kotlin.ir.util.file
 import org.jetbrains.kotlin.ir.util.functions
 import org.jetbrains.kotlin.ir.util.getAllSuperclasses
-import org.jetbrains.kotlin.ir.util.isOverridableOrOverrides
 import org.jetbrains.kotlin.ir.util.kotlinFqName
-import org.jetbrains.kotlin.ir.util.nameForIrSerialization
-import org.jetbrains.kotlin.ir.util.overrides
 import org.jetbrains.kotlin.ir.util.primaryConstructor
 import org.jetbrains.kotlin.ir.util.properties
 import org.jetbrains.kotlin.name.CallableId
-import org.jetbrains.kotlin.name.ClassId
 import org.jetbrains.kotlin.name.FqName
 import org.jetbrains.kotlin.name.Name
-import org.jetbrains.kotlin.types.checker.SimpleClassicTypeSystemContext.getClassFqNameUnsafe
 
 class StarLasuIrGenerationExtension(
     private val messageCollector: MessageCollector,
@@ -111,7 +89,7 @@ class StarLasuIrGenerationExtension(
             )
             val mutableListOf =
                 pluginContext
-                    .referenceFunctions(CallableId(FqName("kotlin.collections"), null, Name.identifier("mutableListOf"))).find { it.owner!!.valueParameters.size == 0 }!!
+                    .referenceFunctions(CallableId(FqName("kotlin.collections"), null, Name.identifier("mutableListOf"))).find { it.owner!!.valueParameters.size == 1 }!!
             function.body = DeclarationIrBuilder(pluginContext, function.symbol).irBlockBody(
                 IrFactoryImpl.createBlockBody(UNDEFINED_OFFSET, UNDEFINED_OFFSET),
             ) {
@@ -120,13 +98,92 @@ class StarLasuIrGenerationExtension(
                 // Then we call apply on it
                 //apply {  }
 
-                val variable = irTemporary(irCall(mutableListOf))
-                +variable
-                val mutableListClass = pluginContext.referenceClass(MutableList::class.classId) ?: throw RuntimeException("MutableList not found")
-                val add = mutableListClass.functions.find { it.owner.name.identifier == "add" } ?: throw RuntimeException("MutableList.add not found")
-                irClass.properties.forEach { property ->
-                    +irCall(add).apply {
-                        dispatchReceiver = irGet(variable)
+                val call = irCall(mutableListOf).apply {
+                    val param = mutableListOf.owner!!.valueParameters.first()
+                    putValueArgument(0, irVararg(param.type, buildList {
+                        irClass.properties.forEach { property ->
+                            if (property.backingField != null) {
+                                val constructor =
+                                    pluginContext.referenceConstructors(FeatureDescription::class.classId).first()
+                                val multiplicity = pluginContext.referenceClass(Multiplicity::class.classId)!!
+                                val multiplicityValueOf =
+                                    multiplicity.functions.find { it.owner.name.identifier == "valueOf" }!!
+                                val featureType = pluginContext.referenceClass(FeatureType::class.classId)!!
+                                val featureTypeValueOf =
+                                    multiplicity.functions.find { it.owner.name.identifier == "valueOf" }!!
+
+
+                                add(irCallConstructor(constructor, emptyList()).apply {
+                                    // val name: String,
+                                    // val provideNodes: Boolean,
+                                    // val multiplicity: Multiplicity,
+                                    // val valueProvider: () -> Any?,
+                                    // val featureType: FeatureType,
+                                    // val derived: Boolean = false,
+                                    putValueArgument(0, irString(property.name.identifier))
+                                    putValueArgument(1, irBoolean(false))
+                                    putValueArgument(2, irCall(multiplicityValueOf).apply {
+                                        putValueArgument(0, irString("SINGULAR"))
+                                    })
+                                    //                                val getter = irClass.functions.find { it.name.identifier == "get${property.name.identifier.capitalize()}"} !!
+                                    //                                IrSimpleFunctionSymbolImpl().apply {
+                                    //                                    this.
+                                    //                                }
+                                    //val getter = property.getter!!
+                                    //val getter = pluginContext.referenceFunctions(CallableId(irClass.classId!!, Name.identifier("get${property.name.identifier.capitalize()}"))).single()
+                                    //putValueArgument(3, irFunctionReference(irClass.defaultType, getter.symbol))
+
+
+                                    val lambda = context.irFactory.buildFun {
+                                        startOffset = SYNTHETIC_OFFSET
+                                        endOffset = SYNTHETIC_OFFSET
+                                        origin = IrDeclarationOrigin.LOCAL_FUNCTION_FOR_LAMBDA
+                                        name = Name.special("<anonymous>")
+                                        visibility = DescriptorVisibilities.LOCAL
+                                        returnType = pluginContext.irBuiltIns.anyNType
+                                    }.apply {
+                                        parent = function
+                                        require(symbol.owner.file == irClass.file)
+//                                        valueParameters = listOf (
+//                                            buildValueParameter(this) {
+//                                                origin = IrDeclarationOrigin.DEFINED
+//                                                name = Name.identifier("field")
+//                                                index = 0
+//                                                type = irClass.defaultType
+//                                            }
+//                                        )
+                                        body = irBlockBody {
+                                            +irReturn(irGetField(irGet(function.dispatchReceiverParameter!!),
+                                                property.backingField
+                                                    ?: throw IllegalStateException("no backing field for property ${property.name.identifier} in ${irClass.kotlinFqName.asString()}")
+                                            )
+                                            )
+                                        }
+                                    }
+                                    putValueArgument(
+                                        3, IrFunctionExpressionImpl(
+                                            startOffset = SYNTHETIC_OFFSET,
+                                            endOffset = SYNTHETIC_OFFSET,
+                                            type = pluginContext.irBuiltIns.functionN(0)
+                                                .typeWith(pluginContext.irBuiltIns.anyNType),
+                                            origin = IrStatementOrigin.LAMBDA,
+                                            function = lambda
+                                        )
+                                    )
+                                    putValueArgument(4, irCall(featureTypeValueOf).apply {
+                                        putValueArgument(0, irString("CONTAINMENT"))
+                                    })
+                                    putValueArgument(5, irBoolean(false))
+                                })
+                            }
+                        }
+                    }))
+                }
+//                val mutableListClass = pluginContext.referenceClass(MutableList::class.classId) ?: throw RuntimeException("MutableList not found")
+//                val add = mutableListClass.functions.find { it.owner.name.identifier == "add" } ?: throw RuntimeException("MutableList.add not found")
+//                irClass.properties.forEach { property ->
+//                    +irCall(add).apply {
+//                        dispatchReceiver = irGet(variable)
 
 //                        class FeatureDescription(
 //                            val name: String,
@@ -136,11 +193,11 @@ class StarLasuIrGenerationExtension(
 //                            val featureType: FeatureType,
 //                            val derived: Boolean = false,
 
-                            putValueArgument(0, irNull())
-                    }
-                }
+//                            putValueArgument(0, irNull())
+//                    }
+//                }
 
-                +irReturn(irGet(variable))
+                +irReturn(call)
             }
         }
     }
