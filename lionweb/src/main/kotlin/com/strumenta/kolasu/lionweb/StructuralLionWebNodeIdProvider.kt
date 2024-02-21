50c6320a20de3d2a104394f43d77e70d93055348
Federico Tomassetti <federico@strumenta.com>
2024-02-21 12:15:59 +0100

    Introducing NodeIdProvider in core

--- lionweb/src/main/kotlin/com/strumenta/kolasu/lionweb/StructuralLionWebNodeIdProvider.kt
+++ lionweb/src/main/kotlin/com/strumenta/kolasu/lionweb/StructuralLionWebNodeIdProvider.kt
@@ -1,36 +1,22 @@
 package com.strumenta.kolasu.lionweb
 
+import com.strumenta.kolasu.ids.ConstantSourceIdProvider
+import com.strumenta.kolasu.ids.SimpleSourceIdProvider
+import com.strumenta.kolasu.ids.SourceIdProvider
+import com.strumenta.kolasu.ids.StructuralNodeIdProvider
 import com.strumenta.kolasu.model.Node
-import com.strumenta.kolasu.model.Source
-import com.strumenta.kolasu.model.containingProperty
-import com.strumenta.kolasu.model.indexInContainingProperty
 import io.lionweb.lioncore.java.utils.CommonChecks
 
-class ConstantSourceIdProvider(var value: String) : SourceIdProvider {
-    override fun sourceId(source: Source?) = value
-}
-
-class StructuralLionWebNodeIdProvider(var sourceIdProvider: SourceIdProvider = SimpleSourceIdProvider()) :
-    LionWebNodeIdProvider {
+class StructuralLionWebNodeIdProvider(sourceIdProvider: SourceIdProvider = SimpleSourceIdProvider()) :
+    StructuralNodeIdProvider(sourceIdProvider) {
 
     constructor(customSourceId: String) : this(ConstantSourceIdProvider(customSourceId))
 
     override fun id(kNode: Node): String {
-        val id = "${sourceIdProvider.sourceId(kNode.source)}_${kNode.positionalID}"
+        val id = super.id(kNode)
         if (!CommonChecks.isValidID(id)) {
             throw IllegalStateException("An invalid LionWeb Node ID has been produced")
         }
         return id
     }
-
-    private val KNode.positionalID: String
-        get() {
-            return if (this.parent == null) {
-                "root"
-            } else {
-                val cp = this.containingProperty()!!
-                val postfix = if (cp.multiple) "${cp.name}_${this.indexInContainingProperty()!!}" else cp.name
-                "${this.parent!!.positionalID}_$postfix"
-            }
-        }
 }
