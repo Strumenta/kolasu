e06be725ec01ad44cbbfaf798ae013a4ce7ac31b
Federico Tomassetti <federico@strumenta.com>
2024-04-28 15:28:47 +0200

    Export LW Java

--- lionweb/build.gradle.kts
+++ lionweb/build.gradle.kts
@@ -13,7 +13,7 @@ dependencies {
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlinVersion")
     testImplementation(libs.gson)
     testImplementation("io.github.mkfl3x:json-delta:1.3")
-    implementation("io.lionweb.lionweb-java:lionweb-java-2023.1-core:$lionwebVersion")
+    api("io.lionweb.lionweb-java:lionweb-java-2023.1-core:$lionwebVersion")
     api(project(":core"))
     api(project(":ast"))
 
