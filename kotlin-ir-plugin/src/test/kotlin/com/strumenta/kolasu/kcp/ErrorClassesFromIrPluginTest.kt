dedf7530cdfcc325bdac091c5e762d1e4fb38db5
Federico Tomassetti <federico@strumenta.com>
2024-05-05 08:52:34 +0200

    Added test `An Error Class is generated`

---
+++ kotlin-ir-plugin/src/test/kotlin/com/strumenta/kolasu/kcp/ErrorClassesFromIrPluginTest.kt
@@ -0,0 +1,41 @@
+@file:OptIn(ExperimentalCompilerApi::class)
+
+package com.strumenta.kolasu.kcp
+
+import com.tschuchort.compiletesting.KotlinCompilation
+import com.tschuchort.compiletesting.SourceFile
+import org.jetbrains.kotlin.compiler.plugin.ExperimentalCompilerApi
+import org.junit.Test
+import kotlin.test.assertEquals
+
+class ErrorClassesFromIrPluginTest : AbstractIrPluginTest() {
+
+    @Test
+    fun `An Error Class is generated`() {
+        val result =
+            compile(
+                sourceFile =
+                    SourceFile.kotlin(
+                        "main.kt",
+                        """
+          import com.strumenta.kolasu.model.MPNode
+          import com.strumenta.kolasu.language.StarLasuLanguage
+          import kotlin.test.assertEquals
+
+    object LanguageMyTest : StarLasuLanguage("mytest")
+
+    data class MyNode(var p1: Int) : MPNode()
+
+fun main() {
+  val n : MyNode = MyNodeError("I could not create MyNode")
+  assertEquals("MyNode", n.concept.name)
+}
+
+""",
+                    ),
+            )
+        assertEquals(KotlinCompilation.ExitCode.OK, result.exitCode)
+    }
+
+
+}
