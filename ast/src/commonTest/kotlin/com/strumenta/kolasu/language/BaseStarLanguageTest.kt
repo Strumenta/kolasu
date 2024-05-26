60a49b1cf56287a8ccc173014de9981dbdd2adea
Federico Tomassetti <federico@strumenta.com>
2024-05-26 13:01:19 +0200

    Completing BaseStarLanguageTest

--- ast/src/commonTest/kotlin/com/strumenta/kolasu/language/BaseStarLanguageTest.kt
+++ ast/src/commonTest/kotlin/com/strumenta/kolasu/language/BaseStarLanguageTest.kt
@@ -1,5 +1,6 @@
 package com.strumenta.kolasu.language
 
+import com.strumenta.kolasu.model.Multiplicity
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
@@ -9,5 +10,40 @@ class BaseStarLanguageTest {
     fun checkASTNode() {
         assertEquals("ASTNode", BaseStarLasuLanguage.astNode.name)
         assertEquals("com.strumenta.basestarlasulanguage.ASTNode", BaseStarLasuLanguage.astNode.qualifiedName)
+    }
+
+    @Test
+    fun checkIntType() {
+        assertEquals("kotlin.Int", intType.name)
+    }
+
+    @Test
+    fun checkIPossiblyNamed() {
+        assertEquals("IPossiblyNamed", BaseStarLasuLanguage.iPossiblyNamed.name)
+        assertEquals(1, BaseStarLasuLanguage.iPossiblyNamed.allFeatures.size)
+        val nameProp = BaseStarLasuLanguage.iPossiblyNamed.property("name")!!
+        assertEquals("name", nameProp.name)
+        assertEquals(stringType, nameProp.type)
+        assertEquals(Multiplicity.OPTIONAL, nameProp.multiplicity)
+    }
+
+    @Test
+    fun checkINamed() {
+        assertEquals("INamed", BaseStarLasuLanguage.iNamed.name)
+        assertEquals(1, BaseStarLasuLanguage.iNamed.allFeatures.size)
+        val nameProp = BaseStarLasuLanguage.iNamed.property("name")!!
+        assertEquals("name", nameProp.name)
+        assertEquals(stringType, nameProp.type)
+        assertEquals(Multiplicity.SINGULAR, nameProp.multiplicity)
+    }
+
+    @Test
+    fun checkRange() {
+        assertEquals("Range", BaseStarLasuLanguage.range.name)
+    }
+
+    @Test
+    fun checkPoint() {
+        assertEquals("Point", BaseStarLasuLanguage.point.name)
     }
 }
