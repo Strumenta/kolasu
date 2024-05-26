9eec5b1bc0336619ad2617b3fb14b9f42c5057a4
Federico Tomassetti <federico@strumenta.com>
2024-05-26 13:51:23 +0200

    Complete testing Classifier

--- ast/src/commonTest/kotlin/com/strumenta/kolasu/language/ConceptTest.kt
+++ ast/src/commonTest/kotlin/com/strumenta/kolasu/language/ConceptTest.kt
@@ -3,8 +3,9 @@ package com.strumenta.kolasu.language
 import com.strumenta.kolasu.model.Multiplicity
 import kotlin.test.Test
 import kotlin.test.assertEquals
+import kotlin.test.assertFailsWith
 
-class ClassifierTest {
+class ConceptTest {
 
     @Test
     fun testAllContainments() {
@@ -142,5 +143,72 @@ class ClassifierTest {
         assertEquals("MyRef", c.reference("MyRef")?.name)
         assertEquals(null, c.reference("MyCont3")?.name)
         assertEquals(null, c.feature("Unexisting")?.name)
+    }
+
+    @Test
+    fun testRequireProperty() {
+        val l = StarLasuLanguage("my.language")
+        val sc = Concept(l, "SC")
+        val i1 = ConceptInterface(l, "I1")
+        val i2 = ConceptInterface(l, "I2")
+        val c = Concept(l, "C")
+        c.superConcept = sc
+        c.conceptInterfaces.add(i1)
+        c.conceptInterfaces.add(i2)
+        i1.declaredFeatures.add(Property("MyProp", false, stringType, { TODO() }))
+        i2.declaredFeatures.add(Property("MyProp2", false, stringType, { TODO() }))
+        c.declaredFeatures.add(Containment("MyCont", Multiplicity.MANY, c, { TODO() }))
+        c.declaredFeatures.add(Reference("MyRef", false, c, { TODO() }))
+        c.declaredFeatures.add(Containment("MyCont2", Multiplicity.MANY, c, { TODO() }))
+        sc.declaredFeatures.add(Containment("MyCont3", Multiplicity.MANY, c, { TODO() }))
+
+        assertEquals("MyProp", c.requireProperty("MyProp")?.name)
+        assertEquals("MyProp2", c.requireProperty("MyProp2")?.name)
+        assertFailsWith<IllegalArgumentException> { c.requireProperty("MyCont") }
+        assertFailsWith<IllegalArgumentException> { c.requireProperty("Unexisting") }
+    }
+
+    @Test
+    fun testRequireContainment() {
+        val l = StarLasuLanguage("my.language")
+        val sc = Concept(l, "SC")
+        val i1 = ConceptInterface(l, "I1")
+        val i2 = ConceptInterface(l, "I2")
+        val c = Concept(l, "C")
+        c.superConcept = sc
+        c.conceptInterfaces.add(i1)
+        c.conceptInterfaces.add(i2)
+        i1.declaredFeatures.add(Property("MyProp", false, stringType, { TODO() }))
+        i2.declaredFeatures.add(Property("MyProp2", false, stringType, { TODO() }))
+        c.declaredFeatures.add(Containment("MyCont", Multiplicity.MANY, c, { TODO() }))
+        c.declaredFeatures.add(Reference("MyRef", false, c, { TODO() }))
+        c.declaredFeatures.add(Containment("MyCont2", Multiplicity.MANY, c, { TODO() }))
+        sc.declaredFeatures.add(Containment("MyCont3", Multiplicity.MANY, c, { TODO() }))
+
+        assertEquals("MyCont", c.requireContainment("MyCont")?.name)
+        assertFailsWith<IllegalArgumentException> { c.requireContainment("MyProp") }
+        assertFailsWith<IllegalArgumentException> { c.requireContainment("Unexisting") }
+    }
+
+    @Test
+    fun testRequireReference() {
+        val l = StarLasuLanguage("my.language")
+        val sc = Concept(l, "SC")
+        val i1 = ConceptInterface(l, "I1")
+        val i2 = ConceptInterface(l, "I2")
+        val c = Concept(l, "C")
+        c.superConcept = sc
+        c.conceptInterfaces.add(i1)
+        c.conceptInterfaces.add(i2)
+        i1.declaredFeatures.add(Property("MyProp", false, stringType, { TODO() }))
+        i2.declaredFeatures.add(Property("MyProp2", false, stringType, { TODO() }))
+        c.declaredFeatures.add(Containment("MyCont", Multiplicity.MANY, c, { TODO() }))
+        c.declaredFeatures.add(Reference("MyRef", false, c, { TODO() }))
+        c.declaredFeatures.add(Containment("MyCont2", Multiplicity.MANY, c, { TODO() }))
+        sc.declaredFeatures.add(Containment("MyCont3", Multiplicity.MANY, c, { TODO() }))
+
+        assertEquals("MyRef", c.requireReference("MyRef")?.name)
+        assertFailsWith<IllegalArgumentException> { c.requireReference("MyProp") }
+        assertFailsWith<IllegalArgumentException> { c.requireReference("Unexisting") }
     }
 }
