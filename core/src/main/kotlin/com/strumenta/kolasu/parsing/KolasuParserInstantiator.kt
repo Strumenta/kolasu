af88922901a01851c55b3c70d847f10b350a22e1
Lorenzo Addazi <lorenzo.addazi@strumenta.com>
2024-06-27 22:26:45 +0200

    Closes #359 - Adds an ASTParserInstantiator interface relaxing the constraints imposed by KolasuParserInstantiator
    Signed-off-by: Lorenzo Addazi <lorenzo.addazi@strumenta.com>

--- core/src/main/kotlin/com/strumenta/kolasu/parsing/KolasuParserInstantiator.kt
+++ core/src/main/kotlin/com/strumenta/kolasu/parsing/KolasuParserInstantiator.kt
@@ -5,8 +5,9 @@ import java.io.File
 /**
  * This interface is used when we need to configure parsers. For example, it can be used for testing, when we need
  * to instantiate a parser with a particular configuration (e.g., specifying include lists of files).
+ * With respect to [ASTParserInstantiator] this interface can be used to create more specific [KolasuParser] instances.
  */
-interface KolasuParserInstantiator {
-    fun instantiate(fileToParse: File): KolasuParser<*, *, *, *>
-    fun instantiate(code: String): KolasuParser<*, *, *, *>
+interface KolasuParserInstantiator : ASTParserInstantiator {
+    override fun instantiate(fileToParse: File): KolasuParser<*, *, *, *>
+    override fun instantiate(code: String): KolasuParser<*, *, *, *>
 }
