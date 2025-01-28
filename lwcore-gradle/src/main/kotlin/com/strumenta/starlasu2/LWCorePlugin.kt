d9ff024ea13c6907268a5fec11ac79a9294b8420
ftomassetti <federico@strumenta.com>
2025-01-28 21:03:25 +0100

    WIP

--- lwcore-gradle/src/main/kotlin/com/strumenta/starlasu2/LWCorePlugin.kt
+++ lwcore-gradle/src/main/kotlin/com/strumenta/starlasu2/LWCorePlugin.kt
@@ -1,8 +1,11 @@
 package com.strumenta.starlasu2
 
+import io.lionweb.lioncore.java.language.Language
+import io.lionweb.lioncore.java.serialization.SerializationProvider
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.Task
+import java.io.File
 
 val tasksGroup = "starlasu"
 val genASTClasses = "generateASTClasses"
@@ -18,7 +21,7 @@ class LWCorePlugin : Plugin<Project> {
      * Prepare the plugin configuration setting in place default values.
      */
     private fun prepareConfiguration(project: Project) : LWCoreGradleExtension {
-        val configuration = project.extensions.create("starlasu", LWCoreGradleExtension::class.java)
+        val configuration = project.extensions.create("starlasu2", LWCoreGradleExtension::class.java)
         return configuration
     }
 
@@ -26,14 +29,26 @@ class LWCorePlugin : Plugin<Project> {
         return project.tasks.create(genASTClasses) {
             this.group = tasksGroup
             this.description = "Generate Kolasu ASTs from LionWeb languages"
-            this.inputs.files(emptyList<Any>())
-            this.outputs.dir(emptyList<Any>())
+            //this.inputs.files(emptyList<Any>())
+            //this.outputs.dir(emptyList<Any>())
             this.doLast {
                 println("LIonWeb AST Classes generation task - started")
-                println("Exploring: ${project.rootDir.absolutePath}")
+                val starLasuDir = File(File(File(project.projectDir,"src"), "main"), "starlasu")
+                if (starLasuDir.exists() && starLasuDir.isDirectory) {
+                    starLasuDir.listFiles().forEach { file ->
+                        println("Process file: ${file.absolutePath}")
+                        generateASTClassesForLanguage(project, configuration, file)
+                    }
+                }
                 println("LIonWeb AST Classes generation task - completed")
             }
         }
+    }
+
+    private fun generateASTClassesForLanguage(project: Project, configuration: LWCoreGradleExtension, languageFile: File) {
+        val jsonSerialization = SerializationProvider.getStandardJsonSerialization()
+        val language = jsonSerialization.deserializeToNodes(languageFile).filterIsInstance<Language>().first()
+        println("Language: ${language.name}")
     }
 
 }
