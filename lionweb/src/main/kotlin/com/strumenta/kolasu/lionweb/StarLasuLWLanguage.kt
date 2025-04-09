a0acb8e57de5928838ad4b73cee229926deefb71
Federico Tomassetti <federico@strumenta.com>
2025-04-09 08:48:23 +0200

    Update dependencies

--- lionweb/src/main/kotlin/com/strumenta/kolasu/lionweb/StarLasuLWLanguage.kt
+++ lionweb/src/main/kotlin/com/strumenta/kolasu/lionweb/StarLasuLWLanguage.kt
@@ -234,33 +234,13 @@ private fun registerSerializersAndDeserializersInMetamodelRegistry() {
             }
             "L${value.line}:${value.column}"
         }
-    val pointDeserializer: PrimitiveDeserializer<Point> =
-        PrimitiveDeserializer<Point> { serialized ->
-            if (serialized == null) {
-                return@PrimitiveDeserializer null
-            }
-            require(serialized.startsWith("L"))
-            require(serialized.removePrefix("L").isNotEmpty())
-            val parts = serialized.removePrefix("L").split(":")
-            require(parts.size == 2)
-            Point(parts[0].toInt(), parts[1].toInt())
-        }
+
     MetamodelRegistry.addSerializerAndDeserializer(StarLasuLWLanguage.Point, pointSerializer, pointDeserializer)
 
     val positionSerializer = PrimitiveSerializer<Position> { value ->
         "${pointSerializer.serialize((value as Position).start)}-${pointSerializer.serialize(value.end)}"
     }
-    val positionDeserializer = PrimitiveDeserializer<Position> { serialized ->
-        if (serialized == null) {
-            null
-        } else {
-            val parts = serialized.split("-")
-            require(parts.size == 2) {
-                "Position has an expected format: $serialized"
-            }
-            Position(pointDeserializer.deserialize(parts[0]), pointDeserializer.deserialize(parts[1]))
-        }
-    }
+
     MetamodelRegistry.addSerializerAndDeserializer(
         StarLasuLWLanguage.Position,
         positionSerializer,
@@ -272,27 +252,53 @@ private fun registerSerializersAndDeserializersInMetamodelRegistry() {
             kt.category.type + "$" + positionSerializer.serialize(kt.position)
         }
     }
-    val tokensListPrimitiveDeserializer = PrimitiveDeserializer<TokensList?> { serialized ->
-        if (serialized == null) {
-            null
-        } else {
-            val tokens = if (serialized.isEmpty()) {
-                mutableListOf()
-            } else {
-                serialized.split(";").map {
-                    val parts = it.split("$")
-                    require(parts.size == 2)
-                    val category = parts[0]
-                    val position = positionDeserializer.deserialize(parts[1])
-                    KolasuToken(TokenCategory(category), position)
-                }.toMutableList()
-            }
-            TokensList(tokens)
-        }
-    }
+
     val tlpt = MetamodelRegistry.getPrimitiveType(TokensList::class, LIONWEB_VERSION_USED_BY_KOLASU)
         ?: throw IllegalStateException("Unknown primitive type class ${TokensList::class}")
     MetamodelRegistry.addSerializerAndDeserializer(tlpt, tokensListPrimitiveSerializer, tokensListPrimitiveDeserializer)
 }
 
 class TokensList(val tokens: List<KolasuToken>)
+
+val pointDeserializer: PrimitiveDeserializer<Point> =
+    PrimitiveDeserializer<Point> { serialized ->
+        if (serialized == null) {
+            return@PrimitiveDeserializer null
+        }
+        require(serialized.startsWith("L"))
+        require(serialized.removePrefix("L").isNotEmpty())
+        val parts = serialized.removePrefix("L").split(":")
+        require(parts.size == 2)
+        Point(parts[0].toInt(), parts[1].toInt())
+    }
+
+val positionDeserializer = PrimitiveDeserializer<Position> { serialized ->
+    if (serialized == null) {
+        null
+    } else {
+        val parts = serialized.split("-")
+        require(parts.size == 2) {
+            "Position has an expected format: $serialized"
+        }
+        Position(pointDeserializer.deserialize(parts[0]), pointDeserializer.deserialize(parts[1]))
+    }
+}
+
+val tokensListPrimitiveDeserializer = PrimitiveDeserializer<TokensList?> { serialized ->
+    if (serialized == null) {
+        null
+    } else {
+        val tokens = if (serialized.isEmpty()) {
+            mutableListOf()
+        } else {
+            serialized.split(";").map {
+                val parts = it.split("$")
+                require(parts.size == 2)
+                val category = parts[0]
+                val position = positionDeserializer.deserialize(parts[1])
+                KolasuToken(TokenCategory(category), position)
+            }.toMutableList()
+        }
+        TokensList(tokens)
+    }
+}
