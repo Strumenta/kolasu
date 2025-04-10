06f735adca851529c8127bcc7a1fb584de24dae8
Federico Tomassetti <federico@strumenta.com>
2025-04-10 15:15:38 +0200

    Extract Origin, Destination, and Properties from Model.kt

---
+++ core/src/main/kotlin/com/strumenta/kolasu/model/Destination.kt
@@ -0,0 +1,10 @@
+package com.strumenta.kolasu.model
+
+import java.io.Serializable
+
+interface Destination
+
+data class CompositeDestination(val elements: List<Destination>) : Destination, Serializable {
+    constructor(vararg elements: Destination) : this(elements.toList())
+}
+data class TextFileDestination(val position: Position?) : Destination, Serializable
