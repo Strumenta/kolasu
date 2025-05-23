d13cd43d6fa10bd2f5243561e3c63b909ac47495
ftomassetti <federico@strumenta.com>
2025-05-23 21:11:37 +0200

    Move settings to KTS

---
+++ settings.gradle.kts
@@ -0,0 +1,21 @@
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        mavenCentral()
+    }
+    plugins {
+        id("org.gradle.toolchains.foojay-resolver-convention") version "0.7.0"
+    }
+}
+
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.7.0"
+}
+
+rootProject.name = "starlasu-kotlin"
+
+include("core")
+include("semantics")
+include("javalib")
+include("lionweb")
+include("lionwebrepo-client")
