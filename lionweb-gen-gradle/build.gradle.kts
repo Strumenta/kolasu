398bd6a40c9b01c4432fdd71cae222a0b306f7fb
ftomassetti <federico@strumenta.com>
2025-05-23 21:01:24 +0200

    Update JVM, Kotlin, ANTLR, and KSP Version

--- lionweb-gen-gradle/build.gradle.kts
+++ lionweb-gen-gradle/build.gradle.kts
@@ -67,6 +67,6 @@ tasks.findByName("dokkaJavadoc")!!.dependsOn(":lionweb:jar")
 tasks.findByName("dokkaJavadoc")!!.dependsOn(":lionweb-gen:jar")
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_1_8
-    targetCompatibility = JavaVersion.VERSION_1_8
+    sourceCompatibility = JavaVersion.VERSION_11
+    targetCompatibility = JavaVersion.VERSION_11
 }
