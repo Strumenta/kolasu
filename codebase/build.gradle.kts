8691ea1ded1f2f00869f4969bd600cb24089c978
Federico Tomassetti <federico@strumenta.com>
2025-07-17 18:25:13 +0200

    Working on build configuration

--- codebase/build.gradle.kts
+++ codebase/build.gradle.kts
@@ -1,3 +1,6 @@
+import com.vanniktech.maven.publish.SonatypeHost
+
+
 plugins {
     kotlin("jvm")
     alias(libs.plugins.ktlint)
@@ -27,60 +30,50 @@ dependencies {
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlinVersion")
 }
 
-publishing {
-    repositories {
-        maven {
-            val releaseRepo = uri("https://central.sonatype.com/publish/repositories/releases/")
-            val snapshotRepo = uri("https://central.sonatype.com/repository/maven-snapshots/")
-            url = if (isReleaseVersion) releaseRepo else snapshotRepo
-            credentials {
-                username = project.findProperty("mavenCentralUsername") as? String ?: "Unknown user"
-                password = project.findProperty("mavenCentralPassword") as? String ?: "Unknown password"
-            }
-        }
-    }
-    publications.withType<MavenPublication>().configureEach {
-        if (name == "maven") {
-            artifactId = "kolasu-${project.name}"
-            suppressPomMetadataWarningsFor("cliApiElements")
-            suppressPomMetadataWarningsFor("cliRuntimeElements")
-            pom {
-                name.set("kolasu-${project.name}")
-                description.set("Framework to work with AST and building languages. Integrated with ANTLR.")
-                version = project.version.toString()
-                packaging = "jar"
-                url.set("https://github.com/Strumenta/kolasu")
-                scm {
-                    connection.set("scm:git:https://github.com/Strumenta/kolasu.git")
-                    developerConnection.set("scm:git:git@github.com:Strumenta/kolasu.git")
-                    url.set("https://github.com/Strumenta/kolasu.git")
-                }
-                licenses {
-                    license {
-                        name.set("Apache License V2.0")
-                        url.set("https://www.apache.org/licenses/LICENSE-2.0")
-                        distribution.set("repo")
-                    }
-                }
-                developers {
-                    developer {
-                        id.set("ftomassetti")
-                        name.set("Federico Tomassetti")
-                        email.set("federico@strumenta.com")
-                    }
-                    developer {
-                        id.set("alessiostalla")
-                        name.set("Alessio Stalla")
-                        email.set("alessio.stalla@strumenta.com")
-                    }
-                }
-            }
-        }
-    }
-}
+mavenPublishing {
+    coordinates(
+        groupId = project.group as String,
+        artifactId = "kolasu-${project.name}",
+        version = project.version as String,
+    )
 
-signing {
-    sign(publishing.publications)
+    pom {
+        name.set("kolasu-" + project.name)
+        description.set("JFramework to work with AST and building languages. Integrated with ANTLR.")
+        version = project.version as String
+        packaging = "jar"
+        url.set("https://github.com/Strumenta/kolasu")
+
+        scm {
+            connection.set("scm:git:https://github.com/strumenta/kolasu.git")
+            developerConnection.set("scm:git:git@github.com:strumenta/kolasu.git")
+            url.set("https://github.com/strumenta/kolasu.git")
+        }
+
+        licenses {
+            license {
+                name.set("Apache License V2.0")
+                url.set("https://www.apache.org/licenses/LICENSE-2.0")
+                distribution.set("repo")
+            }
+        }
+
+        // The developers entry is strictly required by Maven Central
+        developers {
+            developer {
+                id.set("ftomassetti")
+                name.set("Federico Tomassetti")
+                email.set("federico@strumenta.com")
+            }
+            developer {
+                id.set("alessiostalla")
+                name.set("Alessio Stalla")
+                email.set("alessio.stalla@strumenta.com")
+            }
+        }
+    }
+    publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL, true)
+    signAllPublications()
 }
 
 tasks.named("dokkaJavadoc").configure {
@@ -91,12 +84,15 @@ tasks.named("dokkaJavadoc").configure {
 // Some tasks are created during the configuration, and therefore we need to set the dependencies involving
 // them after the configuration has been completed
 project.afterEvaluate {
-    tasks.named("dokkaJavadocJar") {
-        dependsOn(tasks.named("dokkaJavadoc"))
-    }
+//    tasks.named("dokkaJavadocJar") {
+//        dependsOn(tasks.named("dokkaJavadoc"))
+//    }
     tasks.named("generateMetadataFileForMavenPublication") {
-        dependsOn(tasks.named("dokkaJavadocJar"))
+        //dependsOn(tasks.named("dokkaJavadocJar"))
         dependsOn(tasks.named("javadocJar"))
         dependsOn(tasks.named("sourcesJar"))
+    }
+    tasks.named("dokkaHtml") {
+        dependsOn(tasks.named("compileKotlin"))
     }
 }
--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -1,3 +1,5 @@
+import com.vanniktech.maven.publish.SonatypeHost
+
 plugins {
     kotlin("jvm")
     alias(libs.plugins.ktlint)
@@ -82,78 +84,65 @@ idea {
     }
 }
 
-// Maven Publishing
-publishing {
-    repositories {
-        maven {
-            val releaseRepo = uri("https://central.sonatype.com/publish/repositories/releases/")
-            val snapshotRepo = uri("https://central.sonatype.com/repository/maven-snapshots/")
-            url = if (!version.toString().endsWith("SNAPSHOT")) releaseRepo else snapshotRepo
+mavenPublishing {
+    coordinates(
+        groupId = project.group as String,
+        artifactId = "kolasu-${project.name}",
+        version = project.version as String,
+    )
 
-            credentials {
-                username = project.findProperty("mavenCentralUsername") as? String ?: "Unknown user"
-                password = project.findProperty("mavenCentralPassword") as? String ?: "Unknown password"
+    pom {
+        name.set("kolasu-" + project.name)
+        description.set("JFramework to work with AST and building languages. Integrated with ANTLR.")
+        version = project.version as String
+        packaging = "jar"
+        url.set("https://github.com/Strumenta/kolasu")
+
+        scm {
+            connection.set("scm:git:https://github.com/strumenta/kolasu.git")
+            developerConnection.set("scm:git:git@github.com:strumenta/kolasu.git")
+            url.set("https://github.com/strumenta/kolasu.git")
+        }
+
+        licenses {
+            license {
+                name.set("Apache License V2.0")
+                url.set("https://www.apache.org/licenses/LICENSE-2.0")
+                distribution.set("repo")
+            }
+        }
+
+        // The developers entry is strictly required by Maven Central
+        developers {
+            developer {
+                id.set("ftomassetti")
+                name.set("Federico Tomassetti")
+                email.set("federico@strumenta.com")
+            }
+            developer {
+                id.set("alessiostalla")
+                name.set("Alessio Stalla")
+                email.set("alessio.stalla@strumenta.com")
             }
         }
     }
-
-    publications.withType<MavenPublication>().configureEach {
-        if (name == "maven") {
-            artifactId = "kolasu-${project.name}"
-            suppressPomMetadataWarningsFor("cliApiElements")
-            suppressPomMetadataWarningsFor("cliRuntimeElements")
-            pom {
-                name.set("kolasu-${project.name}")
-                description.set("Framework to work with AST and building languages. Integrated with ANTLR.")
-                version = project.version.toString()
-                packaging = "jar"
-                url.set("https://github.com/Strumenta/kolasu")
-                scm {
-                    connection.set("scm:git:https://github.com/Strumenta/kolasu.git")
-                    developerConnection.set("scm:git:git@github.com:Strumenta/kolasu.git")
-                    url.set("https://github.com/Strumenta/kolasu.git")
-                }
-                licenses {
-                    license {
-                        name.set("Apache License V2.0")
-                        url.set("https://www.apache.org/licenses/LICENSE-2.0")
-                        distribution.set("repo")
-                    }
-                }
-                developers {
-                    developer {
-                        id.set("ftomassetti")
-                        name.set("Federico Tomassetti")
-                        email.set("federico@strumenta.com")
-                    }
-                    developer {
-                        id.set("alessiostalla")
-                        name.set("Alessio Stalla")
-                        email.set("alessio.stalla@strumenta.com")
-                    }
-                }
-            }
-        }
-    }
-}
-
-signing {
-    sign(publishing.publications)
+    publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL, true)
+    signAllPublications()
 }
 
 // Some tasks are created during the configuration, and therefore we need to set the dependencies involving
 // them after the configuration has been completed
 project.afterEvaluate {
-    tasks.named("dokkaJavadocJar") {
-        dependsOn(tasks.named("dokkaJavadoc"))
-    }
-    tasks.named("publishMavenPublicationToMavenRepository") {
-        dependsOn(tasks.named("dokkaJavadocJar"))
-        dependsOn(tasks.named("javadocJar"))
-        dependsOn(tasks.named("sourcesJar"))
-    }
+//    tasks.named("dokkaJavadocJar") {
+//        dependsOn(tasks.named("dokkaJavadoc"))
+//    }
+//    tasks.named("publishMavenPublicationToMavenRepository") {
+//        //dependsOn(tasks.named("dokkaJavadocJar"))
+//        dependsOn(tasks.named("javadocJar"))
+//        dependsOn(tasks.named("sourcesJar"))
+//    }
     tasks.named("signMavenPublication") {
-        dependsOn(tasks.named("dokkaJavadocJar"))
+        //dependsOn(tasks.named("dokkaJavadocJar"))
         dependsOn(tasks.named("javadocJar"))
         dependsOn(tasks.named("sourcesJar"))
     }
